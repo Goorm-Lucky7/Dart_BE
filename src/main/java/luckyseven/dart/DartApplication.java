8d6610c3db2ee2ca2b28b396dc253ac5c39830f4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-28 16:32:34 +0900

    feat: 테스트 엔드포인트 추가

--- src/main/java/luckyseven/dart/DartApplication.java
+++ src/main/java/luckyseven/dart/DartApplication.java
@@ -4,8 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 import luckyseven.dart.global.config.DotenvEnvironmentLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DartApplication {
 
 	public static void main(String[] args) {
@@ -25,5 +28,10 @@ public class DartApplication {
 				DotenvEnvironmentLoader.loadEnv();
 			}
 		}
+	}
+
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "[✅ SUCCESS] Application is up and running!";
 	}
 }
