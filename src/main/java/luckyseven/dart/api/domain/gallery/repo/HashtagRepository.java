e3908557b261da0f75ff0a90a79d8395ac0e0f6d
leedidoo <665479@naver.com>
2024-05-30 18:35:23 +0900

    feat: 갤러리 삭제 시 해시태그도 함께 삭제하기 위해 생성

--- src/main/java/luckyseven/dart/api/domain/gallery/repo/HashtagRepository.java
+++ src/main/java/luckyseven/dart/api/domain/gallery/repo/HashtagRepository.java
@@ -1,8 +1,12 @@
 package luckyseven.dart.api.domain.gallery.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import luckyseven.dart.api.domain.gallery.entity.Gallery;
 import luckyseven.dart.api.domain.gallery.entity.Hashtag;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+	List<Hashtag> findByGallery(Gallery gallery);
 }
