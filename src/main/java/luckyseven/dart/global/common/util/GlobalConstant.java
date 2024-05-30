1bbaa1452035357e493f20f2d1489f20e333736c
leedidoo <665479@naver.com>
2024-05-31 00:30:19 +0900

    feat: 상수 사용을 위한 생성

---
+++ src/main/java/luckyseven/dart/global/common/util/GlobalConstant.java
@@ -0,0 +1,12 @@
+package luckyseven.dart.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GlobalConstant {
+	public static final String BLANK = "";
+	public static final int FIVE = 5;
+	public static final int ZERO = 0;
+	public static final int TWENTY = 20;
+}
