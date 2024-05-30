7f2e071c258c47a47f4ceafc412a6131e3f5f1d9
leedidoo <665479@naver.com>
2024-05-31 00:29:15 +0900

    feat: 메서드 최대한 분리, 다양한 검증을 서비스에서 수행, 매직넘버 사용 삭제

--- src/main/java/luckyseven/dart/api/application/gallery/GalleryService.java
+++ src/main/java/luckyseven/dart/api/application/gallery/GalleryService.java
@@ -1,5 +1,7 @@
 package luckyseven.dart.api.application.gallery;
 
+import static luckyseven.dart.global.common.util.GlobalConstant.*;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -36,14 +38,20 @@ public class GalleryService {
 	private final S3Service s3Service;
 
 	public void createGallery(CreateGalleryDto createGalleryDto, MultipartFile thumbnail,
-		List<MultipartFile> imageFiles) throws IOException {
-		Cost cost = determineCost(createGalleryDto);
-		validateEndDateForPay(cost, createGalleryDto);
-		String thumbnailUrl = s3Service.uploadFile(thumbnail);
-		Gallery gallery = Gallery.create(createGalleryDto, thumbnailUrl, cost);
-		galleryRepository.save(gallery);
-		saveHashtags(createGalleryDto.hashTags(), gallery);
-		saveImages(createGalleryDto.images(), imageFiles, gallery);
+		List<MultipartFile> imageFiles) {
+		try {
+			validateImageCount(createGalleryDto);
+			validateImageFileCount(createGalleryDto, imageFiles);
+			final Cost cost = determineCost(createGalleryDto);
+			validateEndDateForPay(cost, createGalleryDto);
+			String thumbnailUrl = s3Service.uploadFile(thumbnail);
+			final Gallery gallery = Gallery.create(createGalleryDto, thumbnailUrl, cost);
+			galleryRepository.save(gallery);
+			saveHashtags(createGalleryDto.hashTags(), gallery);
+			saveImages(createGalleryDto.images(), imageFiles, gallery);
+		} catch (IOException e) {
+			throw new BadRequestException(ErrorCode.FAIL_INVALID_REQUEST);
+		}
 	}
 
 	public void deleteGallery(DeleteGalleryDto deleteGalleryDto) {
@@ -56,7 +64,8 @@ public class GalleryService {
 
 	private void saveHashtags(List<String> hashTags, Gallery gallery) {
 		if (hashTags != null) {
-			validateHashtags(hashTags);
+			validateHashtagsSize(hashTags);
+			validateHashTagsLength(hashTags);
 			List<Hashtag> hashtags = hashTags.stream()
 				.map(tag -> Hashtag.builder().tag(tag).gallery(gallery).build())
 				.collect(Collectors.toList());
@@ -64,26 +73,52 @@ public class GalleryService {
 		}
 	}
 
-	private void saveImages(List<ImageInfoDto> imageInfoDtos, List<MultipartFile> imageFiles, Gallery gallery) throws
-		IOException {
+	private void saveImages(List<ImageInfoDto> imageInfoDtos, List<MultipartFile> imageFiles, Gallery gallery) {
 		if (imageInfoDtos != null && !imageInfoDtos.isEmpty()) {
 			for (int i = 0; i < imageInfoDtos.size(); i++) {
 				ImageInfoDto imageInfoDto = imageInfoDtos.get(i);
 				MultipartFile imageFile = imageFiles.get(i);
-				String imageUrl = s3Service.uploadFile(imageFile);
-				Image image = Image.builder()
-					.imageUri(imageUrl)
-					.imageTitle(imageInfoDto.imageTitle())
-					.description(imageInfoDto.description())
-					.gallery(gallery)
-					.build();
-				imageRepository.save(image);
+				processImage(imageInfoDto, imageFile, gallery);
 			}
 		}
 	}
 
+	private void processImage(ImageInfoDto imageInfoDto, MultipartFile imageFile, Gallery gallery) {
+		try {
+			String imageUrl = s3Service.uploadFile(imageFile);
+			Image image = createImageEntity(imageInfoDto, imageUrl, gallery);
+			imageRepository.save(image);
+		} catch (IOException e) {
+			throw new BadRequestException(ErrorCode.FAIL_INVALID_REQUEST);
+		}
+	}
+
+	private Image createImageEntity(ImageInfoDto imageInfoDto, String imageUrl, Gallery gallery) {
+		return Image.builder()
+			.imageUri(imageUrl)
+			.imageTitle(imageInfoDto.imageTitle())
+			.description(imageInfoDto.description())
+			.gallery(gallery)
+			.build();
+	}
+
 	private Cost determineCost(CreateGalleryDto createGalleryDto) {
-		return createGalleryDto.fee() == 0 ? Cost.FREE : Cost.PAY;
+		if (createGalleryDto.fee() == ZERO) {
+			return Cost.FREE;
+		}
+		return Cost.PAY;
+	}
+
+	private void validateImageCount(CreateGalleryDto createGalleryDto) {
+		if (createGalleryDto.images().size() > TWENTY) {
+			throw new BadRequestException(ErrorCode.FAIL_EXHIBITION_ITEM_LIMIT_EXCEEDED);
+		}
+	}
+
+	private void validateImageFileCount(CreateGalleryDto createGalleryDto, List<MultipartFile> imageFiles) {
+		if (createGalleryDto.images().size() != imageFiles.size()) {
+			throw new BadRequestException(ErrorCode.FAIL_INVALID_EXHIBITION_ITEM_INFO);
+		}
 	}
 
 	private void validateEndDateForPay(Cost cost, CreateGalleryDto createGalleryDto) {
@@ -92,10 +127,13 @@ public class GalleryService {
 		}
 	}
 
-	private void validateHashtags(List<String> hashTags) {
-		if (hashTags.size() > 5) {
+	private void validateHashtagsSize(List<String> hashTags) {
+		if (hashTags.size() > FIVE) {
 			throw new BadRequestException(ErrorCode.FAIL_HASHTAG_SIZE_EXCEEDED);
 		}
+	}
+
+	private void validateHashTagsLength(List<String> hashTags) {
 		Pattern pattern = Pattern.compile("^[^\\s]{1,10}$");
 		boolean invalidTagFound = hashTags.parallelStream().anyMatch(tag -> !pattern.matcher(tag).matches());
 		if (invalidTagFound) {
