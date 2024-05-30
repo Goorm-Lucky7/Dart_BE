f50bc2a3a4ed371d232cc729495474514b896318
leedidoo <665479@naver.com>
2024-05-30 18:37:26 +0900

    feat: 갤러리 삭제 시 관련 이미지들 모두 삭제하기 위해 추가

--- src/main/java/luckyseven/dart/api/domain/gallery/repo/ImageRepository.java
+++ src/main/java/luckyseven/dart/api/domain/gallery/repo/ImageRepository.java
@@ -1,8 +1,12 @@
 package luckyseven.dart.api.domain.gallery.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import luckyseven.dart.api.domain.gallery.entity.Gallery;
 import luckyseven.dart.api.domain.gallery.entity.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+	List<Image> findByGallery(Gallery gallery);
 }
