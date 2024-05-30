6b8abb52cb336421868e137eb7678053eeba066a
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-30 18:07:52 +0900

    refactor: 벨리데이션 구현

--- src/main/java/luckyseven/dart/dto/payment/request/PaymentCreateDto.java
+++ src/main/java/luckyseven/dart/dto/payment/request/PaymentCreateDto.java
@@ -1,7 +1,13 @@
 package luckyseven.dart.dto.payment.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record PaymentCreateDto(
+
+	@NotNull
 	Long galleryId,
+	@NotBlank(message = "주문 정보는 필수로 입력해 주세요.")
 	String order
 ) {
 }
