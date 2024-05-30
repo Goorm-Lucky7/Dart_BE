64b865ce7c708cbee7c39909873485bf703199ca
leedidoo <665479@naver.com>
2024-05-31 00:28:35 +0900

    feat: 누락된 어노테이션 추가

--- src/main/java/luckyseven/dart/api/domain/gallery/repo/ImageRepository.java
+++ src/main/java/luckyseven/dart/api/domain/gallery/repo/ImageRepository.java
@@ -3,10 +3,12 @@ package luckyseven.dart.api.domain.gallery.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import luckyseven.dart.api.domain.gallery.entity.Gallery;
 import luckyseven.dart.api.domain.gallery.entity.Image;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 	List<Image> findByGallery(Gallery gallery);
 }
