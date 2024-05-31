231610f5afd9850e6f836c42c1c7d33a9cccabb5
Yeeun Shim <ner.hesed@gmail.com>
2024-05-31 16:30:15 +0900

    feat: 멤버 프로필 관련 dto 생성

---
+++ src/main/java/luckyseven/dart/api/dto/member/response/MemberProfileResDto.java
@@ -0,0 +1,12 @@
+package luckyseven.dart.api.dto.member.response;
+
+public record MemberProfileResDto(
+	String email,
+	String nickname,
+	String introduce,
+	String bank,
+	String account,
+	String profileImage
+){
+
+}
