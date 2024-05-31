a6553d20ecee3daefca6caeaa5af0697096cd959
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-31 18:10:38 +0900

    feat: 페이징 최근 생성일 순 조회

--- src/main/java/luckyseven/dart/api/domain/review/repo/ReviewRepository.java
+++ src/main/java/luckyseven/dart/api/domain/review/repo/ReviewRepository.java
@@ -1,10 +1,16 @@
 package luckyseven.dart.api.domain.review.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import luckyseven.dart.api.domain.gallery.entity.Gallery;
 import luckyseven.dart.api.domain.review.entity.Review;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+	//겔러리 최근 생성일 순
+	Page<Review> findAllByGalleryOrderByCreatedAtDesc(Gallery gallery, Pageable pageable);
 }
