ee50f4fe0843667c864c08cc7d4db525e9d2ca29
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-31 11:39:33 +0900

    refactor: 벨리데이션 적용

--- src/main/java/luckyseven/dart/presentation/ReviewController.java
+++ src/main/java/luckyseven/dart/presentation/ReviewController.java
@@ -2,9 +2,11 @@ package luckyseven.dart.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import luckyseven.dart.api.application.review.ReviewService;
 import luckyseven.dart.dto.review.request.ReviewCreateDto;
@@ -16,7 +18,7 @@ public class ReviewController {
 	private final ReviewService reviewService;
 
 	@PostMapping
-	public ResponseEntity<String> create(ReviewCreateDto dto) {
+	public ResponseEntity<String> create(@RequestBody @Valid ReviewCreateDto dto) {
 		reviewService.create(dto);
 
 		return ResponseEntity.ok("OK");
