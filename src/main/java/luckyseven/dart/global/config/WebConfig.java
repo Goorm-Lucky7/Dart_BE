dbd501984724ce2ddc2b7f8cfdbe139bd627a957
Yeeun Shim <ner.hesed@gmail.com>
2024-05-31 10:50:58 +0900

    feat: web 설정 및 security 설정

---
+++ src/main/java/luckyseven/dart/global/config/WebConfig.java
@@ -0,0 +1,32 @@
+package luckyseven.dart.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import luckyseven.dart.global.auth.handler.AuthUserArgumentResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new AuthUserArgumentResolver());
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+			.allowedOrigins(
+				"http://3.37.121.50:8080"
+			)
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
+
+}
