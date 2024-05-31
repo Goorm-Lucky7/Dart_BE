850cf4ea1d4b720dd429e463e6d66dc01b93a8b9
Yeeun Shim <ner.hesed@gmail.com>
2024-05-31 10:52:10 +0900

    feat: base time entity를 extends하기 위한 어노테이션 추가

--- src/main/java/luckyseven/dart/DartApplication.java
+++ src/main/java/luckyseven/dart/DartApplication.java
@@ -4,8 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 import luckyseven.dart.global.config.DotenvEnvironmentLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DartApplication {
 
 	public static void main(String[] args) {
