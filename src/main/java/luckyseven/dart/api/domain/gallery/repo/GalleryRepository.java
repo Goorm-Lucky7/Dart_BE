026ef966696a1026c083dfb88f868b8025e300d2
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-31 11:29:18 +0900

    refactor: 컨벤션에 맞게 어노테이션 적용

--- src/main/java/luckyseven/dart/api/domain/gallery/repo/GalleryRepository.java
+++ src/main/java/luckyseven/dart/api/domain/gallery/repo/GalleryRepository.java
@@ -1,8 +1,10 @@
 package luckyseven.dart.api.domain.gallery.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import luckyseven.dart.api.domain.gallery.entity.Gallery;
 
+@Repository
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
 }
--- src/main/java/luckyseven/dart/api/domain/member/repo/MemberRepository.java
+++ src/main/java/luckyseven/dart/api/domain/member/repo/MemberRepository.java
@@ -1,8 +1,10 @@
 package luckyseven.dart.api.domain.member.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import luckyseven.dart.api.domain.member.entity.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
