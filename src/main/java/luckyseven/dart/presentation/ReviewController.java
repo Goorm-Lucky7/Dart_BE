d4efb8b1d6e56dd168871cbcfe9ba98a36c7b364
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-31 18:11:22 +0900

    feat: 리뷰 조회 컨트롤러 구현

--- src/main/java/luckyseven/dart/presentation/ReviewController.java
+++ src/main/java/luckyseven/dart/presentation/ReviewController.java
@@ -1,15 +1,19 @@
 package luckyseven.dart.presentation;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import luckyseven.dart.api.application.review.ReviewService;
 import luckyseven.dart.dto.review.request.ReviewCreateDto;
+import luckyseven.dart.dto.review.response.PageResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +26,14 @@ public class ReviewController {
 		reviewService.create(dto);
 
 		return ResponseEntity.ok("OK");
+	}
+
+	@GetMapping("/{gallery-id}")
+	public PageResponse readAll(
+		@PathVariable(name = "gallery-id") Long galleryId,
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size
+	) {
+		return reviewService.readAll(galleryId, page, size);
 	}
 }
