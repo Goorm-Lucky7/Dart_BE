3aa9806f339d93cee7671df394a7381f262b4ee5
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-05 11:21:20 +0900

    refactor: 연관관계 수정

--- src/main/java/com/dart/api/domain/review/entity/Review.java
+++ src/main/java/com/dart/api/domain/review/entity/Review.java
@@ -15,7 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -39,11 +39,11 @@ public class Review extends BaseTimeEntity {
 	@Enumerated(EnumType.STRING)
 	private Score score;
 
-	@OneToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "gallery_id")
 	private Gallery gallery;
 
-	@OneToOne(fetch = FetchType.LAZY, orphanRemoval = true)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
