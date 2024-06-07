76e51308e1ef15c617cebd855e580a327bbd7145
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-07 18:13:38 +0900

    feat: 실시간 채팅 전송 기능 구현

--- src/main/java/com/dart/api/application/chat/ChatService.java
+++ src/main/java/com/dart/api/application/chat/ChatService.java
@@ -3,12 +3,15 @@ package com.dart.api.application.chat;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dart.api.domain.auth.entity.AuthUser;
+import com.dart.api.domain.chat.entity.ChatMessage;
 import com.dart.api.domain.chat.entity.ChatRoom;
 import com.dart.api.domain.chat.repository.ChatMessageRepository;
 import com.dart.api.domain.chat.repository.ChatRoomRepository;
 import com.dart.api.domain.gallery.entity.Gallery;
 import com.dart.api.domain.member.entity.Member;
 import com.dart.api.domain.member.repository.MemberRepository;
+import com.dart.api.dto.chat.request.ChatMessageCreateDto;
 import com.dart.api.infrastructure.websocket.MemberSessionRegistry;
 import com.dart.global.error.exception.NotFoundException;
 import com.dart.global.error.exception.UnauthorizedException;
@@ -31,14 +34,14 @@ public class ChatService {
 		chatRoomRepository.save(chatRoom);
 	}
 
-	// @Transactional
-	// public void saveAndSendChatMessage(Long chatRoomId, AuthUser authUser, ChatMessageCreateDto chatMessageCreateDto) {
-	// 	final ChatRoom chatRoom = getChatRoomById(chatRoomId);
-	// 	final Member member = getMemberByEmail(authUser.email());
-	//
-	// 	final ChatMessage chatMessage = ChatMessage.createChatMessage(chatRoom, member, chatMessageCreateDto);
-	// 	chatMessageRepository.save(chatMessage);
-	// }
+	@Transactional
+	public void saveAndSendChatMessage(Long chatRoomId, AuthUser authUser, ChatMessageCreateDto chatMessageCreateDto) {
+		final ChatRoom chatRoom = getChatRoomById(chatRoomId);
+		final Member member = getMemberByEmail(authUser.email());
+
+		final ChatMessage chatMessage = ChatMessage.createChatMessage(chatRoom, member, chatMessageCreateDto);
+		chatMessageRepository.save(chatMessage);
+	}
 
 	private ChatRoom getChatRoomById(Long chatRoomId) {
 		return chatRoomRepository.findById(chatRoomId)
