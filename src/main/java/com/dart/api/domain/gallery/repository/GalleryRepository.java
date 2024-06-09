ad87d12eef4541ee25531f3afd7e74c174f5b5da
leedidoo <665479@naver.com>
2024-06-09 20:44:37 +0900

    feat: 갤러리 전체 조회 시 필터 추가 구현으로 인한 기존에 생성해뒀던 코드 삭제

--- src/main/java/com/dart/api/domain/gallery/repository/GalleryRepository.java
+++ src/main/java/com/dart/api/domain/gallery/repository/GalleryRepository.java
@@ -1,13 +1,10 @@
 package com.dart.api.domain.gallery.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dart.api.domain.gallery.entity.Gallery;
 
 @Repository
-public interface GalleryRepository extends JpaRepository<Gallery, Long> {
-	Page<Gallery> findAllByIsPaidTrue(Pageable pageable);
+public interface GalleryRepository extends JpaRepository<Gallery, Long>, GalleryCustomRepository {
 }
