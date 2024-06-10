a131917e76b5026a1323e6f60937bfc4093df9eb
Yeeun Shim <ner.hesed@gmail.com>
2024-06-10 20:40:33 +0900

    feat: 로그인 및 토큰 재발급 메소드 구현

--- src/main/java/com/dart/api/application/auth/AuthenticationService.java
+++ src/main/java/com/dart/api/application/auth/AuthenticationService.java
@@ -3,21 +3,29 @@ package com.dart.api.application.auth;
 
 import static com.dart.global.common.util.AuthConstant.*;
 
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import com.dart.api.application.RedisService;
+import com.dart.api.domain.auth.entity.AuthUser;
 import com.dart.api.domain.member.entity.Member;
 import com.dart.api.domain.member.repository.MemberRepository;
-import com.dart.api.dto.auth.TokenReqDto;
-import com.dart.api.dto.auth.TokenResDto;
 import com.dart.api.dto.member.request.LoginReqDto;
-import com.dart.api.dto.member.response.LoginResDto;
 import com.dart.global.error.exception.BadRequestException;
 import com.dart.global.error.exception.NotFoundException;
+import com.dart.global.error.exception.UnauthorizedException;
 import com.dart.global.error.model.ErrorCode;
 
 @Slf4j
@@ -28,27 +36,41 @@ public class AuthenticationService {
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
 	private final JwtProviderService jwtProviderService;
+	private final RedisService redisService;
+
+	@Value("${jwt.refresh-expire}")
+	private long refreshTokenExpire;
 
 	@Transactional
-	public LoginResDto login(LoginReqDto loginReqDto, HttpServletResponse response) {
+	public void login(LoginReqDto loginReqDto, HttpServletResponse response) {
 		final Member member = findByMemberEmail(loginReqDto.email());
 		validatePasswordMatch(loginReqDto.password(), member.getPassword());
 
-		final String accessToken = jwtProviderService.generateToken(member.getEmail(), member.getNickname(),
-			member.getProfileImageUrl());
+		final String accessToken = jwtProviderService.generateAccessToken(member.getEmail(), member.getNickname(), member.getProfileImageUrl());
+		final String refreshToken = jwtProviderService.generateRefreshToken(member.getEmail());
 
-		response.setHeader(ACCESS_TOKEN_HEADER, accessToken);
+		redisService.setValues(loginReqDto.email(), refreshToken, Duration.ofDays(7));
 
-		return new LoginResDto(accessToken);
+		setTokensInResponse(response, accessToken, refreshToken);
 	}
 
-	public TokenResDto reissue(TokenReqDto tokenReqDto, HttpServletResponse response) {
-		final String newAccessToken = jwtProviderService.reGenerateToken(tokenReqDto.expiredToken());
+	public void reissue(HttpServletRequest request, HttpServletResponse response) {
+		String accessToken = request.getHeader(ACCESS_TOKEN_HEADER).replace("Bearer ", "");
+		String refreshToken = getCookieValue(request, "Refresh-Token");
 
-		response.setHeader(ACCESS_TOKEN_HEADER, newAccessToken);
-		response.setStatus(HttpServletResponse.SC_CREATED);
+		validateRefreshToken(refreshToken);
 
-		return new TokenResDto(newAccessToken);
+		final AuthUser authUser = jwtProviderService.extractAuthUserByAccessToken(accessToken);
+		validateSavedRefreshToken(authUser.email(), refreshToken);
+
+		Member member = getMemberByEmail(authUser.email());
+
+		String newAccessToken = jwtProviderService.generateAccessToken(member.getEmail(), member.getNickname(), member.getProfileImageUrl());
+		String newRefreshToken = jwtProviderService.generateRefreshToken(authUser.email());
+
+		redisService.setValues(authUser.email(), newRefreshToken, Duration.ofMillis(refreshTokenExpire));
+
+		setTokensInResponse(response, newAccessToken, newRefreshToken);
 	}
 
 	private Member findByMemberEmail(String email) {
@@ -60,5 +82,47 @@ public class AuthenticationService {
 		if (!passwordEncoder.matches(password, encodedPassword)) {
 			throw new BadRequestException(ErrorCode.FAIL_INCORRECT_PASSWORD);
 		}
+	}
+
+	private void validateRefreshToken(String refreshToken) {
+		if (!jwtProviderService.isUsable(refreshToken)) {
+			throw new UnauthorizedException(ErrorCode.FAIL_INVALID_TOKEN);
+		}
+	}
+
+	private void validateSavedRefreshToken(String email, String refreshToken) {
+		String savedRefreshToken = redisService.getValues(email);
+		if (!savedRefreshToken.equals(refreshToken)) {
+			throw new UnauthorizedException(ErrorCode.FAIL_INVALID_TOKEN);
+		}
+	}
+
+	private Member getMemberByEmail(String email) {
+		return memberRepository.findByEmail(email)
+			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_MEMBER_NOT_FOUND));
+	}
+
+	private void setTokensInResponse(HttpServletResponse response, String accessToken, String refreshToken) {
+		response.setHeader(ACCESS_TOKEN_HEADER, accessToken);
+
+		ResponseCookie refreshTokenCookie = ResponseCookie.from("Refresh-Token", refreshToken)
+			.httpOnly(true)
+			.secure(true)
+			.path("/")
+			.maxAge(Duration.ofMillis(refreshTokenExpire).toSeconds())
+			.build();
+
+		response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+	}
+
+	private String getCookieValue(HttpServletRequest request, String name) {
+		if (request.getCookies() != null) {
+			for (Cookie cookie : request.getCookies()) {
+				if (cookie.getName().equals(name)) {
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
 	}
 }
