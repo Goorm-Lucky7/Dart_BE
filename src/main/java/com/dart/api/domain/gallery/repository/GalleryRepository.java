32570d44f2642dcbfd9c82fc498f2441108572f9
leedidoo <665479@naver.com>
2024-06-10 22:08:44 +0900

    feat: 갤러리 정보 조회 시 isPaid가 true인 갤러리만 받아오기 위해 생성

--- src/main/java/com/dart/api/domain/gallery/repository/GalleryRepository.java
+++ src/main/java/com/dart/api/domain/gallery/repository/GalleryRepository.java
@@ -1,5 +1,7 @@
 package com.dart.api.domain.gallery.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,5 +12,7 @@ import com.dart.api.domain.gallery.entity.Gallery;
 public interface GalleryRepository extends JpaRepository<Gallery, Long>, GalleryCustomRepository {
 	@Query("SELECT g.isPaid FROM Gallery g WHERE g.id = :id")
 	boolean findIsPaidById(Long id);
+
+	Optional<Gallery> findByIdAndIsPaidTrue(Long id);
 
 }
