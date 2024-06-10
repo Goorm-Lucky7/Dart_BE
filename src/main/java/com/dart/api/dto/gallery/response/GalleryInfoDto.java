9cdcccbe7a2697caed2faa361faf0c5e0acff3fb
leedidoo <665479@naver.com>
2024-06-10 22:07:53 +0900

    feat: 갤러리 정보 조회 응답 DTO 생성

---
+++ src/main/java/com/dart/api/dto/gallery/response/GalleryInfoDto.java
@@ -0,0 +1,19 @@
+package com.dart.api.dto.gallery.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record GalleryInfoDto(
+	String thumbnail,
+	String nickname,
+	String profileImage,
+	String title,
+	String content,
+	LocalDateTime startDate,
+	LocalDateTime endDate,
+	Integer fee,
+	float reviewAverage,
+	boolean hasTicket,
+	List<String> hashtags
+) {
+}
