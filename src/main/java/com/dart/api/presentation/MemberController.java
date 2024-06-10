c108572416eaf45dc938d66b28cec60feabfb8ad
Yeeun Shim <ner.hesed@gmail.com>
2024-06-10 21:52:59 +0900

    feat: path variable에 nickname 추가

--- src/main/java/com/dart/api/presentation/MemberController.java
+++ src/main/java/com/dart/api/presentation/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -52,10 +53,11 @@ public class MemberController {
 		return ResponseEntity.ok(authenticationService.login(loginReqDto, response));
 	}
 
-	@GetMapping("/members")
+	@GetMapping("/members/{nickname}")
 	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<MemberProfileResDto> getMemberProfile(@Auth AuthUser authUser) {
-		return ResponseEntity.ok( memberService.getMemberProfile(authUser));
+	public ResponseEntity<MemberProfileResDto> getMemberProfile(
+		@PathVariable(name = "nickname") String nickname, @Auth AuthUser authUser) {
+		return ResponseEntity.ok( memberService.getMemberProfile(nickname, authUser));
 	}
 
 	@PutMapping("/members")
