b8c1bf0bc03dbe9ed6bb089cb83f32010828766a
leedidoo <665479@naver.com>
2024-06-10 22:10:24 +0900

    feat: 갤러리 정보 조회 시 ticket이 있는지 확인하기 위해 추가

--- src/main/java/com/dart/api/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/dart/api/domain/payment/repository/PaymentRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.dart.api.domain.gallery.entity.Gallery;
 import com.dart.api.domain.member.entity.Member;
+import com.dart.api.domain.payment.entity.Order;
 import com.dart.api.domain.payment.entity.Payment;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 	//결제정보 최근 승인일 순
 	Page<Payment> findAllByMemberOrderByApprovedAtDesc(Member member, Pageable pageable);
+
+	boolean existsByMemberAndGalleryAndOrder(Member member, Gallery gallery, Order order);
 }
