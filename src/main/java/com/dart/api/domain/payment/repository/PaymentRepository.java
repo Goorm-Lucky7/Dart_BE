e1c3066ef2a8580168d38ad29200c06499901084
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-10 23:06:40 +0900

    feat: 결제 시 검증 추가

--- src/main/java/com/dart/api/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/dart/api/domain/payment/repository/PaymentRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dart.api.domain.member.entity.Member;
+import com.dart.api.domain.payment.entity.Order;
 import com.dart.api.domain.payment.entity.Payment;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 	//결제정보 최근 승인일 순
 	Page<Payment> findAllByMemberOrderByApprovedAtDesc(Member member, Pageable pageable);
+
+	boolean existsByMemberAndGalleryIdAndOrder(Member member, Long galleryId, Order order);
+
 }
