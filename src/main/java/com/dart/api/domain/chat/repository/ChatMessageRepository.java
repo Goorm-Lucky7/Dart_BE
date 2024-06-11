dc8f66ab7c07ee7af2d15502cd0ad07a5b7e5308
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-11 20:41:00 +0900

    feat: 채팅방 내 존재하는 채팅메시지 조회 로직 구현

--- src/main/java/com/dart/api/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/dart/api/domain/chat/repository/ChatMessageRepository.java
@@ -1,11 +1,15 @@
 package com.dart.api.domain.chat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dart.api.domain.chat.entity.ChatMessage;
+import com.dart.api.domain.chat.entity.ChatRoom;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
+	List<ChatMessage> findByChatRoom(ChatRoom chatRoom);
 }
