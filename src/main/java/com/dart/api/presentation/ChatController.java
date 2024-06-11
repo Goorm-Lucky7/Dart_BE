c6133b274bc3b1a09319523ce504bfaa6cda0aa5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-11 11:28:16 +0900

    feat: 채팅방에 로그인된 사용자 정보를 조회하는 로직 구현

--- src/main/java/com/dart/api/presentation/ChatController.java
+++ src/main/java/com/dart/api/presentation/ChatController.java
@@ -1,14 +1,20 @@
 package com.dart.api.presentation;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dart.api.application.chat.ChatService;
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
+import com.dart.api.infrastructure.websocket.MemberSessionRegistry;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class ChatController {
 
 	private final ChatService chatService;
+	private final MemberSessionRegistry memberSessionRegistry;
 	private final SimpMessageSendingOperations simpMessageSendingOperations;
 
 	@MessageMapping(value = "/ws/{chat-room-id}/chat-messages")
@@ -27,5 +34,10 @@ public class ChatController {
 	) {
 		chatService.saveAndSendChatMessage(chatRoomId, chatMessageCreateDto, simpMessageHeaderAccessor);
 		simpMessageSendingOperations.convertAndSend("/sub/ws/" + chatRoomId, chatMessageCreateDto.content());
+	}
+
+	@GetMapping("/api/chat-rooms/{chat-room-id}/members")
+	public ResponseEntity<List<String>> getLoggedInVisitors(@PathVariable("chat-room-id") Long chatRoomId) {
+		return ResponseEntity.ok(memberSessionRegistry.getMembersInChatRoom("/sub/ws/" + chatRoomId));
 	}
 }
