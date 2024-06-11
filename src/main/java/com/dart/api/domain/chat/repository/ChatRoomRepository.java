089a28e7cf9d3da2ad0c937fd75f24722b7382f8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-11 20:41:22 +0900

    feat: 전시회 내 채팅방 조회 로직 구현

--- src/main/java/com/dart/api/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/dart/api/domain/chat/repository/ChatRoomRepository.java
@@ -1,11 +1,15 @@
 package com.dart.api.domain.chat.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dart.api.domain.chat.entity.ChatRoom;
+import com.dart.api.domain.gallery.entity.Gallery;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
+	Optional<ChatRoom> findByGallery(Gallery gallery);
 }
