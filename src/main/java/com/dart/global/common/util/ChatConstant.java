36d56d5c3582cb817117524b3f31cc97f53ec8bd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-11 18:46:44 +0900

    feat: 채팅 관련 상수 관리 클래스 선언

---
+++ src/main/java/com/dart/global/common/util/ChatConstant.java
@@ -0,0 +1,10 @@
+package com.dart.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChatConstant {
+
+	public static final String CHAT_SESSION_USER = "authUser";
+}
