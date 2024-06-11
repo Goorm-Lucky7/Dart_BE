ec0a76e0f3face6904d9c6d9723edb1765b62bee
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-11 15:54:28 +0900

    refactor: CORS 설정 추가

--- src/main/java/com/dart/global/config/WebConfig.java
+++ src/main/java/com/dart/global/config/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
 			.allowedOrigins(
+				"http://localhost:5173",
 				"https://localhost:5173"
 			)
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
