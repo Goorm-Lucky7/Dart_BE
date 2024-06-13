545e30dbf8afd876e7858d61c2f8b80d9e0d8f17
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-13 22:34:41 +0900

    feat: 쿠폰 엔티티 생성

---
+++ src/main/java/com/dart/api/domain/coupon/entity/Coupon.java
@@ -0,0 +1,39 @@
+package com.dart.api.domain.coupon.entity;
+
+import com.dart.global.common.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_coupon")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon extends BaseTimeEntity {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "stock", nullable = false)
+	private int stock;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	@Column(name = "coupon_type")
+	@Enumerated(EnumType.STRING)
+	private CouponType couponType;
+}
