e7518b1d37311b160fc236c9743d2db73d153a5e
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-13 22:34:50 +0900

    feat: 쿠폰 지갑 엔티티 생성

---
+++ src/main/java/com/dart/api/domain/coupon/entity/CouponWallet.java
@@ -0,0 +1,35 @@
+package com.dart.api.domain.coupon.entity;
+
+import com.dart.api.domain.member.entity.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_coupon_wallet")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CouponWallet {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "coupon_id")
+	private Coupon coupon;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+}
