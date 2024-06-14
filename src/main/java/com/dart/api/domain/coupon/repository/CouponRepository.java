433e3872957cff3a4c06625bd3d6ffec157d5990
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-14 10:13:19 +0900

    feat: 레포지토리 어노테이션 추가

--- src/main/java/com/dart/api/domain/coupon/repository/CouponRepository.java
+++ src/main/java/com/dart/api/domain/coupon/repository/CouponRepository.java
@@ -1,8 +1,10 @@
 package com.dart.api.domain.coupon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dart.api.domain.coupon.entity.Coupon;
 
+@Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 }
