f9a47842875d9314f92206c8e7f2282f7b3712ed
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-14 15:45:35 +0900

    refactor: Hash 방식과 Key-Value 방식 Redis 저장 로직 분리

--- src/main/java/com/dart/api/infrastructure/redis/HashRedisRepository.java
+++ src/main/java/com/dart/api/infrastructure/redis/HashRedisRepository.java
@@ -1,11 +1,9 @@
 package com.dart.api.infrastructure.redis;
 
-import java.time.Duration;
 import java.util.Map;
 
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,37 +15,18 @@ public class HashRedisRepository {
 
 	private final StringRedisTemplate redisTemplate;
 
-	public String get(String key) {
-		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-		return valueOperations.get(key);
-	}
-
-	public boolean exists(String key) {
-		return Boolean.TRUE.equals(redisTemplate.hasKey(key));
-	}
-
-	public void setExpire(String key, String value, long duration) {
-		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-		Duration expireDuration = Duration.ofSeconds(duration);
-		valueOperations.set(key, value, expireDuration);
-	}
-
-	public void delete(String key) {
-		redisTemplate.delete(key);
-	}
-
-	public void setHashOps(String key, Map<String, String> data) {
+	public void saveHashEntries(String key, Map<String, String> data) {
 		HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
 		values.putAll(key, data);
 	}
 
 	@Transactional(readOnly = true)
-	public String getHashOps(String key, String hashKey) {
+	public String getHashEntry(String key, String hashKey) {
 		HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
-		return Boolean.TRUE.equals(values.hasKey(key, hashKey)) ? (String) values.get(key, hashKey) : "";
+		return Boolean.TRUE.equals(values.hasKey(key, hashKey)) ? (String)values.get(key, hashKey) : "";
 	}
 
-	public void deleteHashOps(String key, String hashKey) {
+	public void deleteHashEntry(String key, String hashKey) {
 		HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
 		values.delete(key, hashKey);
 	}
