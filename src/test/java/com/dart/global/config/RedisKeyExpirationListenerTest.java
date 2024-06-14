7d8db91ca5b8d94808b18900553e2f1a07e111f5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-14 12:41:29 +0900

    test: 채팅 메시지 만료 기간이 되면 자동 삭제되는 로직 테스트 코드 작성

---
+++ src/test/java/com/dart/global/config/RedisKeyExpirationListenerTest.java
@@ -0,0 +1,150 @@
+package com.dart.global.config;
+
+import static com.dart.api.infrastructure.redis.RedisConstant.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.redis.connection.DefaultMessage;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.dart.api.application.gallery.ImageService;
+import com.dart.api.domain.chat.entity.ChatRoom;
+import com.dart.api.domain.chat.repository.ChatRedisRepository;
+import com.dart.api.domain.chat.repository.ChatRoomRepository;
+import com.dart.api.domain.gallery.entity.Gallery;
+import com.dart.api.domain.gallery.repository.GalleryRepository;
+import com.dart.api.domain.gallery.repository.HashtagRepository;
+import com.dart.global.error.exception.NotFoundException;
+import com.dart.support.ChatFixture;
+import com.dart.support.GalleryFixture;
+
+@ExtendWith(SpringExtension.class)
+class RedisKeyExpirationListenerTest {
+
+	@Mock
+	private GalleryRepository galleryRepository;
+
+	@Mock
+	private ImageService imageService;
+
+	@Mock
+	private HashtagRepository hashtagRepository;
+
+	@Mock
+	private ChatRoomRepository chatRoomRepository;
+
+	@Mock
+	private ChatRedisRepository chatRedisRepository;
+
+	@InjectMocks
+	private RedisKeyExpirationListener redisKeyExpirationListener;
+
+	@Mock
+	private RedisMessageListenerContainer redisMessageListenerContainer;
+
+	@BeforeEach
+	void setUp() {
+		redisKeyExpirationListener = new RedisKeyExpirationListener(redisMessageListenerContainer, galleryRepository,
+			imageService, hashtagRepository, chatRoomRepository, chatRedisRepository);
+	}
+
+	@Test
+	@DisplayName("HANDLE EXPIRED GALLERY(⭕️ SUCCESS): 성공적으로 만료된 전시회 KEY값을 처리했습니다.")
+	void handleExpiredGallery_void_success() {
+		// GIVEN
+		Long galleryId = 1L;
+		String expiredKey = REDIS_PAYMENT_PREFIX + galleryId;
+		byte[] body = expiredKey.getBytes(StandardCharsets.UTF_8);
+		Message message = new DefaultMessage(body, body);
+
+		Gallery gallery = GalleryFixture.createGalleryEntity();
+		ChatRoom chatRoom = ChatFixture.createChatRoomEntity();
+
+		when(galleryRepository.findById(galleryId)).thenReturn(Optional.of(gallery));
+		when(chatRoomRepository.findByGallery(gallery)).thenReturn(Optional.of(chatRoom));
+
+		// WHEN
+		redisKeyExpirationListener.onMessage(message, null);
+
+		// THEN
+		verify(chatRoomRepository).delete(chatRoom);
+		verify(imageService).deleteImagesByGallery(gallery);
+		verify(imageService).deleteThumbnail(gallery);
+		verify(hashtagRepository).deleteAll(any());
+		verify(galleryRepository).delete(gallery);
+	}
+
+	@Test
+	@DisplayName("HANDLE EXPIRED GALLERY(❌ FAILURE): 전시회 KEY값이 만료되어 전시회를 찾을 수 없습니다.")
+	void handleExpiredGallery_gallery_NotFoundException_fail() {
+		// GIVEN
+		Long galleryId = 1L;
+
+		when(galleryRepository.findById(galleryId)).thenReturn(Optional.empty());
+
+		// WHEN & THEN
+		assertThatThrownBy(() -> redisKeyExpirationListener.handleExpiredGallery(galleryId))
+			.isInstanceOf(NotFoundException.class)
+			.hasMessage("[❎ ERROR] 요청하신 전시관을 찾을 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("HANDLE EXPIRED GALLERY(❌ FAILURE): 전시회 KEY값이 만료되어 채팅방을 찾을 수 없습니다.")
+	void handleExpiredGallery_chatRoom_NotFoundException_fail() {
+		// GIVEN
+		Long galleryId = 1L;
+
+		Gallery gallery = GalleryFixture.createGalleryEntity();
+		when(galleryRepository.findById(galleryId)).thenReturn(Optional.of(gallery));
+		when(chatRoomRepository.findByGallery(gallery)).thenReturn(Optional.empty());
+
+		// WHEN & THEN
+		assertThatThrownBy(() -> redisKeyExpirationListener.handleExpiredGallery(galleryId))
+			.isInstanceOf(NotFoundException.class)
+			.hasMessage("[❎ ERROR] 요청하신 채팅방을 찾을 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("HANDLE EXPIRED CHAT MESSAGES(⭕️ SUCCESS): 성공적으로 만료된 채팅메시지 KEY값을 처리했습니다.")
+	void handleExpiredChatMessages_void_success() {
+		// GIVEN
+		Long chatRoomId = 1L;
+		String expiredKey = REDIS_CHAT_MESSAGE_PREFIX + chatRoomId;
+		byte[] body = expiredKey.getBytes(StandardCharsets.UTF_8);
+		Message message = new DefaultMessage(body, body);
+
+		// WHEN
+		redisKeyExpirationListener.onMessage(message, null);
+
+		// THEN
+		verify(chatRedisRepository).deleteChatMessages(chatRoomId);
+	}
+
+	@Test
+	@DisplayName("HANDLE UNRELATED KEY(⭕️ SUCCESS): 성공적으로 관련 없는 KEY값을 무시했습니다.")
+	void handleUnrelatedKey_void_success() {
+		// GIVEN
+		String unrelatedKey = "unrelatedKey";
+		byte[] body = unrelatedKey.getBytes(StandardCharsets.UTF_8);
+		Message message = new DefaultMessage(body, body);
+
+		// WHEN
+		redisKeyExpirationListener.onMessage(message, null);
+
+		// THEN
+		verifyNoInteractions(galleryRepository, imageService, hashtagRepository, chatRoomRepository,
+			chatRedisRepository);
+	}
+}
