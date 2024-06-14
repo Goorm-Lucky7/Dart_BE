20a214a5a56e3727f3b86d44edbf268269c35cc7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-14 14:19:15 +0900

    refactor: 클래스명 수정

--- src/main/java/com/dart/api/infrastructure/redis/EmailRedisRepository.java
+++ src/main/java/com/dart/api/infrastructure/redis/EmailRedisRepository.java
@@ -1,6 +1,6 @@
 package com.dart.api.infrastructure.redis;
 
-import static com.dart.api.infrastructure.redis.RedisConstant.REDIS_EMAIL_PREFIX;
+import static com.dart.global.common.util.RedisConstant.REDIS_EMAIL_PREFIX;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -10,28 +10,28 @@ import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class RedisEmailRepository {
+public class EmailRedisRepository {
 
 	@Value("${spring.mail.auth-code-expiration-millis}")
 	private int authCodeExpirationMillis;
 
-	private final RedisHashRepository redisHashRepository;
+	private final HashRedisRepository hashRedisRepository;
 
 	public void setEmail(String key, String data) {
-		redisHashRepository.setExpire(REDIS_EMAIL_PREFIX + key, data, authCodeExpirationMillis/1000);
+		hashRedisRepository.setExpire(REDIS_EMAIL_PREFIX + key, data, authCodeExpirationMillis/1000);
 	}
 
 	@Transactional(readOnly = true)
 	public String getEmail(String key) {
-		String value = redisHashRepository.get(REDIS_EMAIL_PREFIX + key);
+		String value = hashRedisRepository.get(REDIS_EMAIL_PREFIX + key);
 		return value != null ? value : "false";
 	}
 
 	public void deleteEmail(String key) {
-		redisHashRepository.delete(REDIS_EMAIL_PREFIX + key);
+		hashRedisRepository.delete(REDIS_EMAIL_PREFIX + key);
 	}
 
 	public boolean checkExistsEmail(String key) {
-		return redisHashRepository.exists(REDIS_EMAIL_PREFIX + key);
+		return hashRedisRepository.exists(REDIS_EMAIL_PREFIX + key);
 	}
 }
