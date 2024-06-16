6a72280d545af20910128b108ee9091730f2107a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-16 22:49:14 +0900

    test: 전시회 엔티티 생성 로직 수정

--- src/test/java/com/dart/support/GalleryFixture.java
+++ src/test/java/com/dart/support/GalleryFixture.java
@@ -3,8 +3,13 @@ package com.dart.support;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.dart.api.domain.gallery.entity.Cost;
 import com.dart.api.domain.gallery.entity.Gallery;
+import com.dart.api.domain.member.entity.Member;
 import com.dart.api.dto.gallery.request.CreateGalleryDto;
 import com.dart.api.dto.gallery.request.ImageInfoDto;
 
@@ -17,6 +22,45 @@ public class GalleryFixture {
 			Cost.FREE,
 			MemberFixture.createMemberEntity()
 		);
+	}
+
+	public static Gallery createGalleryEntity(Member member) {
+		return Gallery.builder()
+			.title("D'ART Gallery")
+			.content("This is D'ART Gallery")
+			.thumbnail("https://example.com/thumbnail.jpg")
+			.startDate(LocalDateTime.now())
+			.endDate(LocalDateTime.now().plusDays(10))
+			.cost(Cost.FREE)
+			.fee(0)
+			.member(member)
+			.build();
+	}
+
+	public static Gallery createFreeGalleryEntity() {
+		return Gallery.builder()
+			.title("D'ART Gallery")
+			.content("This is D'ART Gallery")
+			.thumbnail("https://example.com/thumbnail.jpg")
+			.startDate(LocalDateTime.now())
+			.endDate(null)
+			.cost(Cost.FREE)
+			.fee(0)
+			.member(MemberFixture.createMemberEntity())
+			.build();
+	}
+
+	public static Gallery createPaidGalleryEntity(long daysUntilEnd) {
+		return Gallery.builder()
+			.title("D'ART Gallery")
+			.content("This is D'ART Gallery")
+			.thumbnail("https://example.com/thumbnail.jpg")
+			.startDate(LocalDateTime.now())
+			.endDate(LocalDateTime.now().plusDays(daysUntilEnd))
+			.cost(Cost.PAY)
+			.fee(1000)
+			.member(MemberFixture.createMemberEntity())
+			.build();
 	}
 
 	public static CreateGalleryDto createGalleryEntityForCreateGalleryDto() {
@@ -32,5 +76,32 @@ public class GalleryFixture {
 				new ImageInfoDto("image2.jpg", "Image 2"))
 			)
 			.build();
+	}
+
+	public static MultipartFile createMultipartFileForThumbnail() {
+		return new MockMultipartFile(
+			"thumbnail",
+			"thumbnail.png",
+			MediaType.IMAGE_PNG_VALUE,
+			"Thumbnail Content".getBytes()
+		);
+	}
+
+	public static List<MultipartFile> createMultipartFileForImages() {
+		MockMultipartFile imageFile1 = new MockMultipartFile(
+			"imageFiles",
+			"image1.png",
+			MediaType.IMAGE_PNG_VALUE,
+			"Image1 Content".getBytes()
+		);
+
+		MockMultipartFile imageFile2 = new MockMultipartFile(
+			"imageFiles",
+			"image2.png",
+			MediaType.IMAGE_PNG_VALUE,
+			"Image2 Content".getBytes()
+		);
+
+		return List.of(imageFile1, imageFile2);
 	}
 }
