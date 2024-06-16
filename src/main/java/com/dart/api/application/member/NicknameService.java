42dfad257920c28b565bb6fd4e6fb7bb3260638d
Yeeun Shim <ner.hesed@gmail.com>
2024-06-17 00:13:04 +0900

    feat: 닉네임 중복확인 로직 추가

--- src/main/java/com/dart/api/application/member/NicknameService.java
+++ src/main/java/com/dart/api/application/member/NicknameService.java
@@ -1,20 +1,14 @@
 package com.dart.api.application.member;
 
-import static com.dart.global.common.util.AuthConstant.*;
-import static com.dart.global.common.util.GlobalConstant.*;
 import static java.lang.Boolean.*;
 
-import java.util.UUID;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import com.dart.api.domain.member.repository.MemberRepository;
-import com.dart.api.infrastructure.redis.RedisNicknameRepository;
-import com.dart.api.infrastructure.redis.RedisSessionRepository;
+import com.dart.api.domain.auth.repository.NicknameRedisRepository;
+import com.dart.api.domain.auth.repository.SessionRedisRepository;
 import com.dart.global.common.util.CookieUtil;
 import com.dart.global.error.exception.ConflictException;
 import com.dart.global.error.model.ErrorCode;
@@ -24,49 +18,60 @@ import com.dart.global.error.model.ErrorCode;
 public class NicknameService {
 
 	private final MemberRepository memberRepository;
-	private final RedisNicknameRepository redisNicknameRepository;
-	private final RedisSessionRepository redisSessionRepository;
+	private final NicknameRedisRepository nicknameRedisRepository;
+	private final SessionRedisRepository sessionRedisRepository;
 
 	private final CookieUtil cookieUtil;
 
 	public void checkAndReserveNickname(String newNickname, String sessionId, HttpServletResponse response) {
+		setSessionIdIfAbsent(response, sessionId);
+
+		String oldNickname = sessionRedisRepository.findNicknameBySessionId(sessionId);
+		removeOldNicknameIfChanged(oldNickname, newNickname);
+
+		verifyNewNickname(newNickname, oldNickname);
+
+		reserveNickname(newNickname, sessionId);
+	}
+	public void setNicknameVerified(String nickname) {
+		if (nicknameRedisRepository.isReserved(nickname)) {
+			nicknameRedisRepository.setVerified(nickname);
+		}
+	}
+
+	private void setSessionIdIfAbsent(HttpServletResponse response, String sessionId) {
 		if (sessionId == null || sessionId.isEmpty()) {
 			cookieUtil.setSessionCookie(response);
 		}
-
-		String oldNickname = redisSessionRepository.findNicknameBySessionId(sessionId);
-		if (oldNickname != null && !oldNickname.equals(newNickname)) {
-			redisNicknameRepository.deleteNickname(oldNickname);
-		}
-
-		verifyNicknameUsed(newNickname);
-		if(oldNickname != null && !oldNickname.equals(newNickname)) {
-			verifyNicknameReserved(newNickname);
-		}
-		reserveNickname(newNickname, sessionId);
 	}
 
-	public void setNicknameVerified(String nickname) {
-		if(redisNicknameRepository.isReserved(nickname)){
-			redisNicknameRepository.setVerified(nickname);
+	private void removeOldNicknameIfChanged(String oldNickname, String newNickname) {
+		if (oldNickname != null && !oldNickname.equals(newNickname)) {
+			nicknameRedisRepository.deleteNickname(oldNickname);
+		}
+	}
+
+	private void verifyNewNickname(String newNickname, String oldNickname) {
+		verifyNicknameUsed(newNickname);
+		if (oldNickname != null && !oldNickname.equals(newNickname)) {
+			verifyNicknameReserved(newNickname);
 		}
 	}
 
 	private void verifyNicknameUsed(String nickname) {
 		if (memberRepository.existsByNickname(nickname)) {
 			throw new ConflictException(ErrorCode.FAIL_NICKNAME_CONFLICT);
-
 		}
 	}
 
 	private void verifyNicknameReserved(String nickname) {
-		if (redisNicknameRepository.isReserved(nickname) == TRUE) {
+		if (nicknameRedisRepository.isReserved(nickname) == TRUE) {
 			throw new ConflictException(ErrorCode.FAIL_NICKNAME_CONFLICT);
 		}
 	}
 
 	private void reserveNickname(String nickname, String sessionId) {
-		redisNicknameRepository.setNickname(nickname);
-		redisSessionRepository.saveSessionNicknameMapping(sessionId, nickname);
+		nicknameRedisRepository.setNickname(nickname);
+		sessionRedisRepository.saveSessionNicknameMapping(sessionId, nickname);
 	}
 }
