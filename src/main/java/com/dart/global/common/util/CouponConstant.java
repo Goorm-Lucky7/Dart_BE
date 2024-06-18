672d38d55039b44124e3b50958ae5a3245d6fd41
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-18 15:16:32 +0900

    feat: 쿠폰 상수 추가

---
+++ src/main/java/com/dart/global/common/util/CouponConstant.java
@@ -0,0 +1,11 @@
+package com.dart.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CouponConstant {
+	public static final int ONE_SECOND = 1000;
+	public static final int TEM_PERSON = 10;
+	public static final int TWO_DAYS = 172800;
+}
