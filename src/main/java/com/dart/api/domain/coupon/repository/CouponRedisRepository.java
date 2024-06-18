604e1092437a3144d32e9b65989a92605025aa07
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-18 15:18:36 +0900

    feat: 쿠폰 레디스 레포지토리 메소드 추가

--- src/main/java/com/dart/api/domain/coupon/repository/CouponRedisRepository.java
+++ src/main/java/com/dart/api/domain/coupon/repository/CouponRedisRepository.java
@@ -1,11 +1,16 @@
 package com.dart.api.domain.coupon.repository;
 
+import static com.dart.global.common.util.CouponConstant.*;
 import static com.dart.global.common.util.RedisConstant.*;
+import static java.util.Objects.*;
 
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
+import com.dart.api.infrastructure.redis.ValueRedisRepository;
 import com.dart.api.infrastructure.redis.ZSetRedisRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,24 +19,49 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CouponRedisRepository {
 	private final ZSetRedisRepository zSetRedisRepository;
+	private final ValueRedisRepository valueRedisRepository;
 
-	public void addIfAbsentQueue(Long couponId, Long memberId, double registerTime) {
+	public void addIfAbsentQueue(Long couponId, String email, double registerTime) {
 		zSetRedisRepository.addElementIfAbsent(
 			REDIS_COUPON_PREFIX + couponId.toString(),
-			memberId.toString(),
+			email,
 			registerTime,
-			36000
+			TWO_DAYS
 		);
 	}
 
-	public boolean hasValue(Long couponId, Long memberId) {
+	public boolean hasValue(Long couponId, String email) {
 		return Objects.nonNull(
-			zSetRedisRepository.score(REDIS_COUPON_PREFIX + couponId.toString(), memberId.toString()));
+			zSetRedisRepository.score(REDIS_COUPON_PREFIX + couponId.toString(), email));
 	}
 
 	public int sizeQueue(Long couponId) {
 		return zSetRedisRepository
 			.size(REDIS_COUPON_PREFIX + couponId.toString())
 			.intValue();
+	}
+
+	public int getCount(Long couponId) {
+		String couponCountKey = REDIS_COUPON_PREFIX + couponId.toString();
+		String count = valueRedisRepository.getValue(couponCountKey);
+
+		if (isNull(count)) {
+			return 0;
+		}
+
+		return Integer.parseInt(count);
+	}
+
+	public Set<String> rangeQueue(Long couponId, long start, long end) {
+		return zSetRedisRepository
+			.getRange(REDIS_COUPON_PREFIX + couponId.toString(), start, end)
+			.stream()
+			.map(String.class::cast)
+			.collect(Collectors.toSet());
+	}
+
+	public void increase(Long couponId, long count) {
+		final String couponCountKey = REDIS_COUPON_COUNT_PREFIX + couponId;
+		valueRedisRepository.increment(couponCountKey, count);
 	}
 }
