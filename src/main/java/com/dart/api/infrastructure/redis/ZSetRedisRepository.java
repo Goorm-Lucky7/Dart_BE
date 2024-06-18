943252eac53dbc9ce323a65bea83bc69d1bc8282
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-18 21:29:02 +0900

    refactor: 중복된 로직 개선 및 메서드 분리

--- src/main/java/com/dart/api/infrastructure/redis/ZSetRedisRepository.java
+++ src/main/java/com/dart/api/infrastructure/redis/ZSetRedisRepository.java
@@ -20,9 +20,18 @@ public class ZSetRedisRepository {
 		redisTemplate.opsForZSet().add(requireNonNull(key), requireNonNull(value), score);
 	}
 
-	public void addElementIfAbsent(String key, Object value, double score, long expire) {
-		redisTemplate.opsForZSet().addIfAbsent(requireNonNull(key), requireNonNull(value), score);
-		redisTemplate.expire(key, Duration.ofSeconds(expire));
+	public boolean addElementIfAbsent(String key, Object value, double score) {
+		return Boolean.TRUE.equals(
+			redisTemplate.opsForZSet().addIfAbsent(requireNonNull(key), requireNonNull(value), score)
+		);
+	}
+
+	public void addElementWithExpiry(String key, Object value, double score, long expire) {
+		redisTemplate.opsForZSet().add(requireNonNull(key), requireNonNull(value), score);
+
+		if (expire > 0) {
+			redisTemplate.expire(key, Duration.ofDays(expire));
+		}
 	}
 
 	public Set<Object> getRange(String key, long start, long end) {
