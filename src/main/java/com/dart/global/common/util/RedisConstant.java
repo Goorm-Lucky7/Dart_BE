8d7f94f15d1fcdaa7eb42c96aaed4f9080db0237
Yeeun Shim <ner.hesed@gmail.com>
2024-06-19 11:52:54 +0900

    feat: redis 관련 상수 추가

--- src/main/java/com/dart/global/common/util/RedisConstant.java
+++ src/main/java/com/dart/global/common/util/RedisConstant.java
@@ -13,6 +13,7 @@ public class RedisConstant {
 	public static final String REDIS_PAYMENT_PREFIX = "payment:";
 	public static final String REDIS_CHAT_MESSAGE_PREFIX = "chatMessage:";
 	public static final String REDIS_NICKNAME_PREFIX = "nickname:";
+	public static final String REDIS_TRIE_PREFIX = "trie:";
 
 	public static final String REDIS_SESSION_EMAIL_PREFIX = "session-email:";
 	public static final String REDIS_SESSION_NICKNAME_PREFIX = "session-nickname:";
@@ -20,6 +21,10 @@ public class RedisConstant {
 	public static final String CODE = "code";
 	public static final String RESERVED = "reserved";
 	public static final String VERIFIED = "verified";
+	public static final String IS_END_OF_WORD = "is-end-of-word";
+	public static final String TITLE = "title";
+	public static final String AUTHOR = "author";
+	public static final String HASHTAG = "hashtag";
 
 	public static final long EMAIL_VERIFICATION_EXPIRATION_TIME_SECONDS = THIRTY_MINUTES;
 	public static final long NICKNAME_VERIFICATION_EXPIRATION_TIME_SECONDS = THIRTY_MINUTES;
