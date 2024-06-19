151161ba6129fa92f9681f35074eb395f12a784d
Yeeun Shim <ner.hesed@gmail.com>
2024-06-19 11:55:16 +0900

    feat: 로그인 여부 관계 없이 검색하도록 설정 추가

--- src/main/java/com/dart/global/config/SecurityConfig.java
+++ src/main/java/com/dart/global/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
 			.requestMatchers(HttpMethod.GET, "/api/mypage").permitAll()
 			.requestMatchers(HttpMethod.GET, "/api/members").permitAll()
 			.requestMatchers(HttpMethod.GET, "/api/reviews/info").permitAll()
+			.requestMatchers(HttpMethod.GET, "/api/autocomplete").permitAll()
 			.anyRequest().authenticated()
 		);
 
