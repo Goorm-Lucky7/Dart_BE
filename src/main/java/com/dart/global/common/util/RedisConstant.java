266d6b0ba4499e61e1428164717743c0bae94979
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-19 11:14:36 +0900

    refactor: 깃 충돌 해결

--- src/main/java/com/dart/global/common/util/RedisConstant.java
+++ src/main/java/com/dart/global/common/util/RedisConstant.java
@@ -1,5 +1,7 @@
 package com.dart.global.common.util;
 
+import static com.dart.global.common.util.GlobalConstant.*;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +12,23 @@ public class RedisConstant {
 	public static final String REDIS_GALLERY_PREFIX = "gallery:";
 	public static final String REDIS_PAYMENT_PREFIX = "payment:";
 	public static final String REDIS_CHAT_MESSAGE_PREFIX = "chatMessage:";
+	public static final String REDIS_NICKNAME_PREFIX = "nickname:";
 	public static final String REDIS_COUPON_PREFIX = "coupon:";
 	public static final String REDIS_COUPON_COUNT_PREFIX = "coupon_count:";
+
+	public static final String REDIS_SESSION_EMAIL_PREFIX = "session-email:";
+	public static final String REDIS_SESSION_NICKNAME_PREFIX = "session-nickname:";
+
+	public static final long FREE_EXHIBITION_MESSAGE_EXPIRY_DAYS = 30;
+	public final static long ZSET_START_INDEX = 0;
+	public final static long ZSET_END_INDEX_ALL = -1;
+
+	public static final String CODE = "code";
+	public static final String RESERVED = "reserved";
+	public static final String VERIFIED = "verified";
+
+	public static final long EMAIL_VERIFICATION_EXPIRATION_TIME_SECONDS = THIRTY_MINUTES;
+	public static final long NICKNAME_VERIFICATION_EXPIRATION_TIME_SECONDS = THIRTY_MINUTES;
+	public static final int SESSION_EMAIL_EXPIRATION_TIME_SECONDS = THIRTY_MINUTES;
+	public static final int SESSION_NICKNAME_EXPIRATION_TIME_SECONDS = THIRTY_MINUTES;
 }
