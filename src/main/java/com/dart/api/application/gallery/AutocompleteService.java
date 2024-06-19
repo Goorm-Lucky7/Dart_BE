4d1b4f435eec8640aa5260efa0f43fe03cd04ffc
Yeeun Shim <ner.hesed@gmail.com>
2024-06-19 11:54:36 +0900

    feat: autocomplete 코드 추가

---
+++ src/main/java/com/dart/api/application/gallery/AutocompleteService.java
@@ -0,0 +1,24 @@
+package com.dart.api.application.gallery;
+
+import java.util.List;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.dart.api.domain.gallery.repository.TrieRedisRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AutocompleteService {
+
+	private final TrieRedisRepository trieRedisRepository;
+
+	@Cacheable(value = "autocompleteCache", key = "#type + '_' + #prefix")
+	public List<String> autocomplete(String type, String prefix) {
+		List<String>results= trieRedisRepository.search(type, prefix);
+		System.out.println("Autocomplete results: " + results);
+		return results;
+	}
+}
---
+++ src/main/java/com/dart/api/presentation/AutocompleteController.java
@@ -0,0 +1,24 @@
+package com.dart.api.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import com.dart.api.application.gallery.AutocompleteService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/autocomplete")
+public class AutocompleteController {
+
+	private final AutocompleteService autocompleteService;
+
+	@GetMapping
+	public List<String> autocomplete(@RequestParam String type, @RequestParam String prefix) {
+		return autocompleteService.autocomplete(type, prefix);
+	}
+}
