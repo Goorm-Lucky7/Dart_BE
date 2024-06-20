8274d74ab5a90b82243c72409abb2ecb68485b75
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-20 16:11:21 +0900

    test: ZSet에서 List 기반 Redis 저장소 테스트 코드로 수정

--- src/test/java/com/dart/api/domain/chat/repository/ChatRedisRepositoryTest.java
+++ src/test/java/com/dart/api/domain/chat/repository/ChatRedisRepositoryTest.java
@@ -1,13 +1,12 @@
 package com.dart.api.domain.chat.repository;
 
+import static com.dart.global.common.util.ChatConstant.*;
 import static com.dart.global.common.util.RedisConstant.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,15 +16,20 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.dart.api.domain.chat.entity.ChatRoom;
+import com.dart.api.domain.gallery.entity.Gallery;
+import com.dart.api.domain.member.entity.Member;
 import com.dart.api.dto.chat.response.ChatMessageReadDto;
-import com.dart.api.infrastructure.redis.ZSetRedisRepository;
+import com.dart.api.dto.page.PageResponse;
+import com.dart.api.infrastructure.redis.ListRedisRepository;
 import com.dart.support.ChatFixture;
+import com.dart.support.GalleryFixture;
+import com.dart.support.MemberFixture;
 
 @ExtendWith(MockitoExtension.class)
 class ChatRedisRepositoryTest {
 
 	@Mock
-	private ZSetRedisRepository zSetRedisRepository;
+	private ListRedisRepository listRedisRepository;
 
 	@InjectMocks
 	private ChatRedisRepository chatRedisRepository;
@@ -37,56 +41,60 @@ class ChatRedisRepositoryTest {
 		String sender = "testSender";
 		String content = "Hello 👋🏻";
 		LocalDateTime createdAt = LocalDateTime.now();
-		long expirySeconds = 7 * 24 * 60 * 60;
 		boolean isAuthor = false;
 
 		String messageValue = ChatFixture.createMessageValue(sender, content, createdAt, isAuthor);
-		ChatRoom chatRoom = ChatFixture.createChatRoomEntity();
+
+		Member member = MemberFixture.createMemberEntity();
+		Gallery gallery = GalleryFixture.createGalleryEntity(member);
+		ChatRoom chatRoom = ChatFixture.createChatRoomEntity(gallery);
 
 		// WHEN
-		chatRedisRepository.saveChatMessage(chatRoom, content, sender, createdAt, expirySeconds);
+		chatRedisRepository.saveChatMessage(chatRoom, content, sender, createdAt, CHAT_MESSAGE_EXPIRY_SECONDS);
 
 		// THEN
-		verify(zSetRedisRepository).addElementWithExpiry(
+		verify(listRedisRepository, times(1)).addElementWithExpiry(
 			eq(REDIS_CHAT_MESSAGE_PREFIX + chatRoom.getId()),
 			eq(messageValue),
-			doubleThat(value -> value == createdAt.toEpochSecond(ZoneOffset.UTC)),
-			eq(expirySeconds)
+			eq(CHAT_MESSAGE_EXPIRY_SECONDS)
 		);
 	}
 
 	@Test
-	@DisplayName("GET CHAT MESSAGE READ DTO(⭕️ SUCCESS): 성공적으로 채팅 메시지를 조회했습니다.")
+	@DisplayName("GET CHAT MESSAGE READ DTO(⭕️ SUCCESS): 성공적으로 Redis에서 채팅 메시지를 조회했습니다.")
 	void getChatMessageReadDto_void_success() {
 		// GIVEN
 		Long chatRoomId = 1L;
-		long ZSET_START_INDEX = 0;
-		long ZSET_END_INDEX_ALL = -1;
+		int page = 0;
+		int size = 10;
 
-		Set<Object> messageValues = Set.of(
+		long start = (long)page * size;
+		long end = start + size - 1;
+
+		List<Object> messageValues = List.of(
 			ChatFixture.createMessageValue("testSender1", "Hello 👋🏻", LocalDateTime.now(), false),
 			ChatFixture.createMessageValue("testSender2", "Bye 👋🏻", LocalDateTime.now(), false)
 		);
 
-		when(zSetRedisRepository.getRange(
-			eq(REDIS_CHAT_MESSAGE_PREFIX + chatRoomId),
-			eq(ZSET_START_INDEX),
-			eq(ZSET_END_INDEX_ALL))
-		).thenReturn(messageValues);
+		when(listRedisRepository.getRange(REDIS_CHAT_MESSAGE_PREFIX + chatRoomId, start, end))
+			.thenReturn(messageValues);
 
 		// WHEN
-		List<ChatMessageReadDto> actualMessages = chatRedisRepository.getChatMessageReadDto(chatRoomId);
+		PageResponse<ChatMessageReadDto> actualChatMessagePageReadDtoList =
+			chatRedisRepository.getChatMessageReadDto(chatRoomId, page, size);
 
 		// THEN
-		assertEquals(2, actualMessages.size());
-		verify(zSetRedisRepository).getRange(
-			eq(REDIS_CHAT_MESSAGE_PREFIX + chatRoomId),
-			eq(ZSET_START_INDEX),
-			eq(ZSET_END_INDEX_ALL));
+		assertEquals(2, actualChatMessagePageReadDtoList.pages().size());
+
+		assertEquals("testSender1", actualChatMessagePageReadDtoList.pages().get(0).sender());
+		assertEquals("Hello 👋🏻", actualChatMessagePageReadDtoList.pages().get(0).content());
+
+		assertEquals("testSender2", actualChatMessagePageReadDtoList.pages().get(1).sender());
+		assertEquals("Bye 👋🏻", actualChatMessagePageReadDtoList.pages().get(1).content());
 	}
 
 	@Test
-	@DisplayName("DELETE CHAT MESSAGES(⭕️ SUCCESS): 성공적으로 모든 채팅 메시지를 삭제했습니다.")
+	@DisplayName("DELETE CHAT MESSAGES(⭕️ SUCCESS): 성공적으로 Redis에서 모든 채팅 메시지를 삭제했습니다.")
 	void deleteChatMessages_void_success() {
 		// GIVEN
 		Long chatRoomId = 1L;
@@ -95,6 +103,6 @@ class ChatRedisRepositoryTest {
 		chatRedisRepository.deleteChatMessages(chatRoomId);
 
 		// THEN
-		verify(zSetRedisRepository).deleteAllElements(REDIS_CHAT_MESSAGE_PREFIX + chatRoomId);
+		verify(listRedisRepository, times(1)).deleteAllElements(REDIS_CHAT_MESSAGE_PREFIX + chatRoomId);
 	}
 }
