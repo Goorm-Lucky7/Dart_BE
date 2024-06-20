bd709b673ee3a6cb8963230ceda8681f7c570e3b
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-21 00:34:50 +0900

    refactor: 어노테이션 적용

--- src/main/java/com/dart/api/dto/prioritycoupon/request/PriorityCouponPublishDto.java
+++ src/main/java/com/dart/api/dto/prioritycoupon/request/PriorityCouponPublishDto.java
@@ -1,6 +1,9 @@
 package com.dart.api.dto.prioritycoupon.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record PriorityCouponPublishDto(
+	@NotNull
 	Long priorityCouponId
 ) {
 }
