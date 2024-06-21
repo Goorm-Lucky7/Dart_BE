2a29188317d90da021d444f3980de5227cf78fc6
Yeeun Shim <ner.hesed@gmail.com>
2024-06-21 14:47:26 +0900

    feat: 회원정보 수정을 put -> post 변경

--- src/main/java/com/dart/api/presentation/MemberController.java
+++ src/main/java/com/dart/api/presentation/MemberController.java
@@ -8,7 +8,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -69,7 +68,7 @@ public class MemberController {
 		return ResponseEntity.ok(memberService.getMemberProfile(nickname, authUser));
 	}
 
-	@PutMapping("/members")
+	@PostMapping("/members")
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<String> updateMemberProfile(@Auth AuthUser authUser,
 		@RequestPart @Validated MemberUpdateDto memberUpdateDto,
