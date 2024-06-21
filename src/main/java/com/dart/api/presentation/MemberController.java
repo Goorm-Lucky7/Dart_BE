d469f398d747dc30a2a29e89714db4b0e6bc26b3
Yeeun Shim <ner.hesed@gmail.com>
2024-06-21 15:22:12 +0900

    feat: 미디어 파일을 받도록 수정함.

--- src/main/java/com/dart/api/presentation/MemberController.java
+++ src/main/java/com/dart/api/presentation/MemberController.java
@@ -3,11 +3,13 @@ package com.dart.api.presentation;
 import static com.dart.global.common.util.AuthConstant.*;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -68,7 +70,7 @@ public class MemberController {
 		return ResponseEntity.ok(memberService.getMemberProfile(nickname, authUser));
 	}
 
-	@PostMapping("/members")
+	@PutMapping(name = "/members",  consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<String> updateMemberProfile(@Auth AuthUser authUser,
 		@RequestPart @Validated MemberUpdateDto memberUpdateDto,
