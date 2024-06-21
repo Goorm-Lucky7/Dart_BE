2bfb4c2bc16a3ce1a4eba20a8c0287fc10e8a414
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-21 18:06:48 +0900

    refactor: ChannelInterceptor 메서드 리팩토링

--- src/main/java/com/dart/api/infrastructure/websocket/AuthChannelInterceptor.java
+++ src/main/java/com/dart/api/infrastructure/websocket/AuthChannelInterceptor.java
@@ -2,6 +2,9 @@ package com.dart.api.infrastructure.websocket;
 
 import static com.dart.global.common.util.AuthConstant.*;
 import static com.dart.global.common.util.ChatConstant.*;
+import static com.dart.global.common.util.GlobalConstant.*;
+
+import java.util.Objects;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -23,39 +26,41 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AuthChannelInterceptor implements ChannelInterceptor {
 
-	public static final String STOMP_COMMAND_HEADER = "stompCommand";
-
 	private final JwtProviderService jwtProviderService;
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor stompHeaderAccessor = StompHeaderAccessor.wrap(message);
 
-		final String authorizationHeader = String.valueOf(
-			stompHeaderAccessor.getFirstNativeHeader(ACCESS_TOKEN_HEADER));
-
-		final String command = String.valueOf(
-			stompHeaderAccessor.getHeader(STOMP_COMMAND_HEADER));
-
-		if (!command.equals("SEND")) {
+		if (!isSendCommand(stompHeaderAccessor)) {
 			return message;
 		}
 
-		if (authorizationHeader == null || authorizationHeader.equals("null")) {
-			throw new UnauthorizedException(ErrorCode.FAIL_LOGIN_REQUIRED);
-		}
+		final String authorizationHeader = stompHeaderAccessor.getFirstNativeHeader(ACCESS_TOKEN_HEADER);
+		final String token = extractToken(authorizationHeader);
+		validateAuthenticateToken(token);
 
-		final String token = authorizationHeader.substring(7);
-
-		try {
-			jwtProviderService.isUsable(token);
-			final AuthUser authUser = jwtProviderService.extractAuthUserByAccessToken(token);
-			stompHeaderAccessor.setHeader(CHAT_SESSION_USER, authUser);
-		} catch (Exception e) {
-			log.error("FAIL VALIDATE JWT TOKEN: {}", e.getMessage(), e);
-			throw new UnauthorizedException(ErrorCode.FAIL_INVALID_TOKEN);
-		}
+		final AuthUser authUser = jwtProviderService.extractAuthUserByAccessToken(token);
+		stompHeaderAccessor.setHeader(CHAT_SESSION_USER, authUser);
 
 		return message;
+	}
+
+	private boolean isSendCommand(StompHeaderAccessor stompHeaderAccessor) {
+		return Objects.equals(StompCommand.SEND, stompHeaderAccessor.getHeader(STOMP_COMMAND_HEADER));
+	}
+
+	private String extractToken(String authorizationHeader) {
+		if (authorizationHeader == null || !authorizationHeader.startsWith(BEARER)) {
+			return null;
+		}
+		return authorizationHeader.replaceFirst(BEARER, BLANK).trim();
+	}
+
+	private void validateAuthenticateToken(String token) {
+		if (token == null || !jwtProviderService.isUsable(token)) {
+			log.warn("[✅ LOGGER] INVALID OR MISSING JWT TOKEN");
+			throw new UnauthorizedException(ErrorCode.FAIL_TOKEN_EXPIRED_OR_INVALID);
+		}
 	}
 }
