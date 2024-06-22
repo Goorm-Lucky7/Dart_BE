ed9330b4bbb74f8dae085af30df7badac27ad06a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-22 16:17:47 +0900

    feat: 클라이언트 식별자와 이벤트 목록 관리 및 대기 중인 이벤트를 추가하는 로직 구현

---
+++ src/main/java/com/dart/api/application/notification/PendingEventsService.java
@@ -0,0 +1,18 @@
+package com.dart.api.application.notification;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PendingEventsService {
+
+	private final Map<String, List<Object>> pendingEventsDB = new ConcurrentHashMap<>();
+
+	public void addEvent(String clientId, Object event) {
+		pendingEventsDB.computeIfAbsent(clientId, clientIdKey -> new CopyOnWriteArrayList<>()).add(event);
+	}
+}
