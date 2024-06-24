04693d2fa7556bb87a1829bfc3e6ee61e6614f02
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-24 16:15:24 +0900

    feat: SSE 알림 조회 기능 구현

--- src/main/java/com/dart/api/application/notification/SSENotificationService.java
+++ src/main/java/com/dart/api/application/notification/SSENotificationService.java
@@ -1,12 +1,17 @@
 package com.dart.api.application.notification;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.dart.api.domain.auth.entity.AuthUser;
 import com.dart.api.domain.member.repository.MemberRepository;
+import com.dart.api.domain.notification.entity.Notification;
+import com.dart.api.domain.notification.repository.NotificationRepository;
 import com.dart.api.domain.notification.repository.SSESessionRepository;
+import com.dart.api.dto.notification.response.NotificationReadDto;
 import com.dart.global.error.exception.UnauthorizedException;
 import com.dart.global.error.model.ErrorCode;
 
@@ -20,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SSENotificationService {
 
 	private final MemberRepository memberRepository;
+	private final NotificationRepository notificationRepository;
 	private final SSESessionRepository sseSessionRepository;
 
 	public SseEmitter subscribe(AuthUser authUser) {
@@ -32,6 +38,12 @@ public class SSENotificationService {
 		log.info("[✅ LOGGER] DUMMY EVENT SENT TO SSE EMITTER");
 
 		return sseEmitter;
+	}
+
+	public List<NotificationReadDto> getNotifications() {
+		return notificationRepository.findAll().stream()
+			.map(Notification::toNotificationReadDto)
+			.toList();
 	}
 
 	private Long getMemberIdFromAuthUser(AuthUser authUser) {
