856643bb5b56e2c8fc9e438b1a748e1de394606f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-24 16:11:20 +0900

    feat: 실시간 쿠폰 발행 스케쥴러 시간을 설정한 상수 정의

--- src/main/java/com/dart/global/common/util/SSEConstant.java
+++ src/main/java/com/dart/global/common/util/SSEConstant.java
@@ -8,4 +8,5 @@ public class SSEConstant {
 
 	public static final long SSE_DEFAULT_TIMEOUT = 60 * 60 * 1000L;
 	public static final String SSE_EMITTER_EVENT_NAME = "SSE";
+	public static final String DAILY_AT_MIDNIGHT = "0 0 0 * * ?";
 }
