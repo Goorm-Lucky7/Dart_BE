ebfbde2b74ec5ddef60863b6a17e5188c6e526ae
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-24 16:16:42 +0900

    feat: SSE 알림 목록 조회 컨트롤러 구현

--- src/main/java/com/dart/api/presentation/NotificationController.java
+++ src/main/java/com/dart/api/presentation/NotificationController.java
@@ -1,5 +1,7 @@
 package com.dart.api.presentation;
 
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.dart.api.application.notification.SSENotificationService;
 import com.dart.api.domain.auth.entity.AuthUser;
+import com.dart.api.dto.notification.response.NotificationReadDto;
 import com.dart.global.auth.annotation.Auth;
 
 import lombok.RequiredArgsConstructor;
@@ -23,5 +26,10 @@ public class NotificationController {
 	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public ResponseEntity<SseEmitter> subscribe(@Auth AuthUser authUser) {
 		return ResponseEntity.ok(SSENotificationService.subscribe(authUser));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<NotificationReadDto>> getNotifications() {
+		return ResponseEntity.ok(SSENotificationService.getNotifications());
 	}
 }
