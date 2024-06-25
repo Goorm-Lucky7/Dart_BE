eead3593e2e2cd35f008e667da98412c1b28c246
ChangMinPark2 <98qkrckdals@naver.com>
2024-06-25 17:48:40 +0900

    feat: 선착순 쿠폰 조회를 위한 jpa 메서드 생성

--- src/main/java/com/dart/api/domain/coupon/repository/PriorityCouponWalletRepository.java
+++ src/main/java/com/dart/api/domain/coupon/repository/PriorityCouponWalletRepository.java
@@ -1,6 +1,7 @@
 package com.dart.api.domain.coupon.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import com.dart.api.domain.member.entity.Member;
 @Repository
 public interface PriorityCouponWalletRepository extends JpaRepository<PriorityCouponWallet, Long> {
 	List<PriorityCouponWallet> findByMemberAndIsUsedFalse(Member member);
+
+	Optional<PriorityCouponWallet> findByIdAndMemberIdAndIsUsedFalse(Long id, Long memberId);
 }
