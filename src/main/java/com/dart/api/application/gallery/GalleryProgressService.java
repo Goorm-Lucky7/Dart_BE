6e69ce0fac05174d1a746c413726e47fa660dea8
leedidoo <665479@naver.com>
2024-06-25 17:13:44 +0900

    feat: 갤러리 생성 과정 조회에 필요한 GalleryProgressService 생성

---
+++ src/main/java/com/dart/api/application/gallery/GalleryProgressService.java
@@ -0,0 +1,42 @@
+package com.dart.api.application.gallery;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.dart.api.domain.notification.repository.SSESessionRepository;
+import com.dart.api.infrastructure.redis.ValueRedisRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GalleryProgressService {
+
+	private final ValueRedisRepository valueRedisRepository;
+	private final SSESessionRepository sseSessionRepository;
+
+	public void addEmitter(Long galleryId, SseEmitter emitter) {
+		sseSessionRepository.addSSEEmitter(galleryId, emitter);
+	}
+
+	public void updateProgress(Long galleryId, int progress) {
+		sseSessionRepository.sendEvent(galleryId, progress, "Progress Update");
+
+		if (progress == 100) {
+			sseSessionRepository.deleteSSEEmitterByClientId(galleryId);
+			deleteProgressFromRedis(galleryId);
+		} else {
+			saveProgressToRedis(galleryId, progress);
+		}
+	}
+
+	public void saveProgressToRedis(Long galleryId, int progress) {
+		String key = "gallery:progress:" + galleryId;
+		valueRedisRepository.saveValue(key, String.valueOf(progress));
+	}
+
+	public void deleteProgressFromRedis(Long galleryId) {
+		String key = "gallery:progress:" + galleryId;
+		valueRedisRepository.deleteValue(key);
+	}
+}
