096162037f44946db7939851ba31609bb2097d31
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-26 09:13:27 +0900

    test: 불필요한 로직 제거

--- src/test/java/com/dart/support/ChatFixture.java
+++ src/test/java/com/dart/support/ChatFixture.java
@@ -1,12 +1,13 @@
 package com.dart.support;
 
+import static com.dart.global.common.util.ChatConstant.*;
+
 import java.time.LocalDateTime;
 
-import com.dart.api.domain.chat.entity.ChatMessage;
 import com.dart.api.domain.chat.entity.ChatRoom;
 import com.dart.api.domain.gallery.entity.Gallery;
 import com.dart.api.dto.chat.request.ChatMessageCreateDto;
-import com.dart.api.dto.chat.response.ChatMessageReadDto;
+import com.dart.api.dto.chat.request.ChatMessageSendDto;
 
 public class ChatFixture {
 
@@ -21,54 +22,22 @@ public class ChatFixture {
 			.build();
 	}
 
-	public static ChatMessage createChatMessageEntity() {
-		return ChatMessage.createChatMessage(
-			createChatRoomEntity(),
-			MemberFixture.createMemberEntity(),
-			createChatMessageEntityForChatMessageCreateDto()
-		);
-	}
-
-	public static ChatMessage createChatMessageEntity(ChatRoom chatRoom) {
-		return ChatMessage.createChatMessage(
-			chatRoom,
-			MemberFixture.createMemberEntity(),
-			createChatMessageEntityForChatMessageCreateDto()
-		);
-	}
-
-	public static ChatMessage createChatMessageEntity(ChatRoom chatRoom, String content) {
-		return ChatMessage.createChatMessage(
-			chatRoom,
-			MemberFixture.createMemberEntity(),
-			createChatMessageEntityForChatMessageCreateDto(content)
-		);
-	}
-
 	public static ChatMessageCreateDto createChatMessageEntityForChatMessageCreateDto() {
 		return ChatMessageCreateDto.builder()
 			.content("Hello 👋🏻")
 			.build();
 	}
 
-	public static ChatMessageCreateDto createChatMessageEntityForChatMessageCreateDto(String content) {
-		return ChatMessageCreateDto.builder()
-			.content(content)
-			.build();
-	}
-
-	public static ChatMessageReadDto createChatMessageReadDto(String sender, String content, LocalDateTime createdAt,
-		boolean isAuthor
+	public static ChatMessageSendDto createChatMessageSendDto(Long memberId, Long chatRoomId, String content,
+		LocalDateTime createdAt, boolean isAuthor
 	) {
-		return ChatMessageReadDto.builder()
-			.sender(sender)
+		return ChatMessageSendDto.builder()
+			.memberId(memberId)
+			.chatRoomId(chatRoomId)
 			.content(content)
 			.createdAt(createdAt)
 			.isAuthor(isAuthor)
+			.expirySeconds(CHAT_MESSAGE_EXPIRY_SECONDS)
 			.build();
-	}
-
-	public static String createMessageValue(String sender, String content, LocalDateTime createdAt, boolean isAuthor) {
-		return sender + "|" + content + "|" + createdAt.toString() + "|" + isAuthor;
 	}
 }
