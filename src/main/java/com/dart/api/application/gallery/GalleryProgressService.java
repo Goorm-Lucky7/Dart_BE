340ec52ffc122ce0a61e6795c5d84cf3bcc80a8b
leedidoo <665479@naver.com>
2024-06-26 21:07:34 +0900

    fix: 전시 생성 과정 조회 시 레디스 사용 제거 후 수정

--- src/main/java/com/dart/api/application/gallery/GalleryProgressService.java
+++ src/main/java/com/dart/api/application/gallery/GalleryProgressService.java
@@ -1,13 +1,16 @@
 package com.dart.api.application.gallery;
 
 import static com.dart.global.common.util.GlobalConstant.*;
-import static com.dart.global.common.util.RedisConstant.*;
+import static com.dart.global.common.util.SSEConstant.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.dart.api.domain.auth.entity.AuthUser;
+import com.dart.api.domain.member.repository.MemberRepository;
 import com.dart.api.domain.notification.repository.SSESessionRepository;
-import com.dart.api.infrastructure.redis.ValueRedisRepository;
+import com.dart.global.error.exception.UnauthorizedException;
+import com.dart.global.error.model.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,31 +18,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GalleryProgressService {
 
-	private final ValueRedisRepository valueRedisRepository;
 	private final SSESessionRepository sseSessionRepository;
+	private final MemberRepository memberRepository;
 
-	public void addEmitter(Long galleryId, SseEmitter emitter) {
-		sseSessionRepository.addSSEEmitter(galleryId, emitter);
+	public SseEmitter createEmitter(AuthUser authUser) {
+		Long clientId = getMemberIdFromAuthUser(authUser);
+		return sseSessionRepository.saveSSEEmitter(clientId, SSE_CREATE_GALLERY_TIMEOUT);
 	}
 
-	public void updateProgress(Long galleryId, int progress) {
-		sseSessionRepository.sendEvent(galleryId, progress, "Progress Update");
-
+	public void sendProgress(Long clientId, int progress) {
+		sseSessionRepository.sendEvent(clientId, progress, "Progress Update");
 		if (progress == ONE_HUNDRED_PERCENT) {
-			sseSessionRepository.deleteSSEEmitterByClientId(galleryId);
-			deleteProgressFromRedis(galleryId);
-		} else {
-			saveProgressToRedis(galleryId, progress);
+			sseSessionRepository.deleteSSEEmitterByClientId(clientId);
 		}
 	}
 
-	public void saveProgressToRedis(Long galleryId, int progress) {
-		String key = REDIS_GALLERY_PREFIX + galleryId;
-		valueRedisRepository.saveValue(key, String.valueOf(progress));
-	}
-
-	public void deleteProgressFromRedis(Long galleryId) {
-		String key = REDIS_GALLERY_PREFIX + galleryId;
-		valueRedisRepository.deleteValue(key);
+	private Long getMemberIdFromAuthUser(AuthUser authUser) {
+		return memberRepository.findByEmail(authUser.email())
+			.orElseThrow(() -> new UnauthorizedException(ErrorCode.FAIL_LOGIN_REQUIRED))
+			.getId();
 	}
 }
