e8ab0a2eaebdada9fc38bae13f13c22e2b38b6ea
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-27 14:24:22 +0900

    refactor: JsonProperty 어노테이션 제거

--- src/main/java/com/dart/api/dto/chat/response/MemberSessionDto.java
+++ src/main/java/com/dart/api/dto/chat/response/MemberSessionDto.java
@@ -1,11 +1,9 @@
 package com.dart.api.dto.chat.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record MemberSessionDto(
-	@JsonProperty("nickname") String nickname,
-	@JsonProperty("sessionId") String sessionId,
-	@JsonProperty("destination") String destination,
-	@JsonProperty("profileImageUrl") String profileImageUrl
+	String nickname,
+	String sessionId,
+	String destination,
+	String profileImageUrl
 ) {
 }
