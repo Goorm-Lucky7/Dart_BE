1488562111b5c1ed604d7d0067eb17c925186785
Yeeun Shim <ner.hesed@gmail.com>
2024-06-29 06:08:52 +0900

    feat: CacheConfig 중복 import statement 제거

--- src/main/java/com/dart/global/config/CacheConfig.java
+++ src/main/java/com/dart/global/config/CacheConfig.java
@@ -13,9 +13,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
