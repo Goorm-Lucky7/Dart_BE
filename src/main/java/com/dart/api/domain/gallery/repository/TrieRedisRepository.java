9c2b2db1ba553843074b6053d8d55bacb513c833
Yeeun Shim <ner.hesed@gmail.com>
2024-06-29 06:10:20 +0900

    feat: 검색어 자동완성을 위한 Repository 생성

--- src/main/java/com/dart/api/domain/gallery/repository/TrieRedisRepository.java
+++ src/main/java/com/dart/api/domain/gallery/repository/TrieRedisRepository.java
@@ -2,12 +2,18 @@ package com.dart.api.domain.gallery.repository;
 
 import static com.dart.global.common.util.RedisConstant.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.dart.api.infrastructure.redis.ValueRedisRepository;
+import com.dart.api.infrastructure.redis.ZSetRedisRepository;
+import com.dart.global.common.util.CharacterSplitter;
+import com.dart.global.error.exception.NotFoundException;
+import com.dart.global.error.model.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,55 +21,80 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TrieRedisRepository {
 
-	private final RedisTemplate<String, Object> redisTemplate;
+	private final ValueRedisRepository valueRedisRepository;
+	private final ZSetRedisRepository zSetRedisRepository;
 
-	public void insert(String type, String word) {
-		String key = REDIS_TRIE_PREFIX + type.toLowerCase();
-		String nodeKey = key;
+	public void insert(String type, String keyword) {
+		keyword = CharacterSplitter.splitString(keyword);
+		for (int i = 1; i <= keyword.length(); i++) {
+			String prefix = keyword.substring(0, i);
+			String key = generateTypePrefixKey(type, prefix);
+			System.out.println("insert key :" + i +" : "+key);
+			Double score = CharacterSplitter.getUnicodeScore(keyword);
 
-		for (char c : word.toCharArray()) {
-			nodeKey += ":" + c;
-			redisTemplate.opsForHash().putIfAbsent(nodeKey, IS_END_OF_WORD, false);
-			System.out.println("Inserted node: " + nodeKey);
+			zSetRedisRepository.addElement(key, keyword, score);
+			valueRedisRepository.increment(REDIS_COUNT_PREFIX + key, 1);
 		}
-		redisTemplate.opsForHash().put(nodeKey, IS_END_OF_WORD, true);
-		System.out.println("Word end at: " + nodeKey);
 	}
 
 	public List<String> search(String type, String prefix) {
-		String key = REDIS_TRIE_PREFIX + type.toLowerCase();
-		String nodeKey = key;
+		String splitPrefix = CharacterSplitter.splitString(prefix);
+		String splitConvertedPrefix = CharacterSplitter.splitAndConvertString(prefix);
 
-		System.out.println("Searching for prefix: " + prefix);
-		for (char c : prefix.toCharArray()) {
-			nodeKey += ":" + c;
-			if (!redisTemplate.hasKey(nodeKey)) {
-				System.out.println("Node not found: " + nodeKey);
-				return List.of();
-			}
-			System.out.println("Found node: " + nodeKey);
-		}
+		String key1 = generateTypePrefixKey(type, splitPrefix);
+		String key2 = generateTypePrefixKey(type, splitConvertedPrefix);
 
-		return findAllWords(nodeKey, new StringBuilder(prefix));
+		SortedSet<String> resultSet1 = new TreeSet<>(zSetRedisRepository.getRange(key1, 0, 9));
+		SortedSet<String> resultSet2 = new TreeSet<>(zSetRedisRepository.getRange(key2, 0, 9));
+
+		SortedSet<String> combinedSet = new TreeSet<>(resultSet1);
+		combinedSet.addAll(resultSet2);
+
+		return combinedSet.stream()
+			.map(CharacterSplitter::mergeKoreanString)
+			.limit(10)
+			.collect(Collectors.toList());
 	}
 
-	private List<String> findAllWords(String nodeKey, StringBuilder prefix) {
-		List<String> results = new ArrayList<>();
-		Boolean isEndOfWord = (Boolean) redisTemplate.opsForHash().get(nodeKey, IS_END_OF_WORD);
+	public void remove(String type, String keyword) {
+		keyword = CharacterSplitter.splitString(keyword);
 
-		if (Boolean.TRUE.equals(isEndOfWord)) {
-			results.add(prefix.toString());
-		}
+		for (int i = 1; i <= keyword.length(); i++) {
+			String prefix = keyword.substring(0, i);
+			String key = generateTypePrefixKey(type, prefix);
 
-		Set<Object> children = redisTemplate.opsForHash().keys(nodeKey);
-		for (Object child : children) {
-			if (!child.equals(IS_END_OF_WORD)) {
-				prefix.append(child.toString().charAt(0));
-				results.addAll(findAllWords(nodeKey + ":" + child, prefix));
-				prefix.deleteCharAt(prefix.length() - 1);
+			valueRedisRepository.increment(REDIS_COUNT_PREFIX + key, -1);
+
+			if(getKeywordCount(key) < 1) {
+				zSetRedisRepository.removeElement(key, keyword);
+				valueRedisRepository.deleteValue(REDIS_COUNT_PREFIX + key);
 			}
 		}
-		System.out.println("Results: " + results);
-		return results;
+	}
+
+	private Long getKeywordCount(String key) {
+		String count = valueRedisRepository.getValue(REDIS_COUNT_PREFIX + key);
+		if (count == null) { return 0L; }
+		else { return Long.valueOf(count); }
+	}
+
+	public boolean exists(String type, String keyword) {
+		String key = generateTypePrefixKey(type, keyword);
+		Double score = zSetRedisRepository.score(key, keyword);
+		return score != null && score > 0;
+	}
+
+	private String generateTypePrefixKey(String type, String prefix) {
+		type = type.trim();
+		switch (type) {
+			case TITLE:
+				return REDIS_TITLE_PREFIX + prefix;
+			case AUTHOR:
+				return REDIS_AUTHOR_PREFIX + prefix;
+			case HASHTAG:
+				return REDIS_HASHTAG_PREFIX + prefix;
+			default:
+				throw new NotFoundException(ErrorCode.FAIL_TYPE_NOT_FOUND);
+		}
 	}
 }
