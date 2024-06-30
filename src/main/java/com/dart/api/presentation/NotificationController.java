afd2fbcc5a5420fbf13d42e6b420e9f082b9bdd8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-30 16:21:16 +0900

    refactor: 알림 목록 조회 API 제거

--- src/main/java/com/dart/api/presentation/NotificationController.java
+++ src/main/java/com/dart/api/presentation/NotificationController.java
@@ -1,7 +1,5 @@
 package com.dart.api.presentation;
 
-import java.util.List;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +9,6 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.dart.api.application.notification.SSENotificationService;
 import com.dart.api.domain.auth.entity.AuthUser;
-import com.dart.api.dto.notification.response.NotificationReadDto;
 import com.dart.global.auth.annotation.Auth;
 
 import lombok.RequiredArgsConstructor;
@@ -26,10 +23,5 @@ public class NotificationController {
 	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public ResponseEntity<SseEmitter> subscribe(@Auth AuthUser authUser) {
 		return ResponseEntity.ok(SSENotificationService.subscribe(authUser));
-	}
-
-	@GetMapping
-	public ResponseEntity<List<NotificationReadDto>> getNotifications() {
-		return ResponseEntity.ok(SSENotificationService.getNotifications());
 	}
 }
