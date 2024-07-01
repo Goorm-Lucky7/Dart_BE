c4f01e05488597be4d75dc9ba14fb17f3f795aa7
Yeeun Shim <ner.hesed@gmail.com>
2024-07-01 17:23:05 +0900

    remove: 미사용 코드 제거

--- src/main/java/com/dart/api/domain/auth/repository/TokenRedisRepository.java
+++ src/main/java/com/dart/api/domain/auth/repository/TokenRedisRepository.java
@@ -14,35 +14,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TokenRedisRepository {
 
-	@Value("${jwt.access-expire}")
-	private long accessTokenExpire;
-
 	@Value("${jwt.refresh-expire}")
 	private long refreshTokenExpire;
 
 	private final ValueRedisRepository valueRedisRepository;
 
-	public void setAccessToken(String key, String data) {
-		valueRedisRepository.saveValueWithExpiry(REDIS_ACCESS_TOKEN_PREFIX + key, data, accessTokenExpire/1000);
-	}
 	public void setRefreshToken(String key, String data) {
 		valueRedisRepository.saveValueWithExpiry(REDIS_REFRESH_TOKEN_PREFIX + key, data, refreshTokenExpire/1000);
-	}
-
-	@Transactional(readOnly = true)
-	public String getAccessToken(String key) {
-		String value = valueRedisRepository.getValue(REDIS_ACCESS_TOKEN_PREFIX + key);
-		return value != null ? value : "false";
 	}
 
 	@Transactional(readOnly = true)
 	public String getRefreshToken(String key) {
 		String value = valueRedisRepository.getValue(REDIS_REFRESH_TOKEN_PREFIX + key);
 		return value != null ? value : "false";
-	}
-
-	public void deleteAccessToken(String key) {
-		valueRedisRepository.deleteValue(REDIS_ACCESS_TOKEN_PREFIX + key);
 	}
 
 	public void deleteRefreshToken(String key) {
