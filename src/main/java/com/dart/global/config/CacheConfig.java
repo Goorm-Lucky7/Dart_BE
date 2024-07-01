0ca838b4281894aeb7e91fff86bf0f5abee08f2c
ChangMinPark2 <98qkrckdals@naver.com>
2024-07-01 12:06:32 +0900

    refactor: cache 설정을 변경

--- src/main/java/com/dart/global/config/CacheConfig.java
+++ src/main/java/com/dart/global/config/CacheConfig.java
@@ -3,16 +3,16 @@ package com.dart.global.config;
 import java.time.Duration;
 import java.time.LocalTime;
 
-
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -35,6 +35,14 @@ public class CacheConfig {
 
 		return RedisCacheManager.builder(redisConnectionFactory)
 			.cacheDefaults(redisCacheConfiguration)
+			.build();
+	}
+
+	@Bean
+	@Primary
+	public RedisCacheManager couponCacheManager(RedisConnectionFactory redisConnectionFactory) {
+		return RedisCacheManager.builder(redisConnectionFactory)
+			.cacheDefaults(redisCacheConfiguration())
 			.build();
 	}
 
