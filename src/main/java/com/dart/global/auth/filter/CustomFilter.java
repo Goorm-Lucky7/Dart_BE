0171744eb13ac9de601f4dc638cf547eea5a7a49
Yeeun Shim <ner.hesed@gmail.com>
2024-07-01 11:57:10 +0900

    fix: 필터 내에 옵션 처리 추가

--- src/main/java/com/dart/global/auth/filter/CustomFilter.java
+++ src/main/java/com/dart/global/auth/filter/CustomFilter.java
@@ -16,6 +16,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 
 @Component
 @Order(1)
@@ -58,8 +59,16 @@ public class CustomFilter extends OncePerRequestFilter {
 	);
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(
+		@NotNull HttpServletRequest request,
+		@NotNull HttpServletResponse response,
+		@NotNull FilterChain filterChain)
 		throws ServletException, IOException {
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+			return;
+		}
+
 		if (!isApiValid(request)) {
 			response.setContentType("application/json;charset=UTF-8");
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
