e393e7dae73a67fb9456d4f9f3c35fc9fc3aa450
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-07-02 14:54:05 +0900

    refactor: 불필요하게 정의된 상수 제거

--- src/main/java/com/dart/global/common/util/SSEConstant.java
+++ src/main/java/com/dart/global/common/util/SSEConstant.java
@@ -10,7 +10,4 @@ public class SSEConstant {
 	public static final long SSE_CREATE_GALLERY_TIMEOUT = 60 * 1000L;
 	public static final String SSE_EMITTER_EVENT_NAME = "SSE";
 	public static final String DAILY_AT_MIDNIGHT = "0 0 0 * * ?";
-	public static final int REEXHIBITION_REQUEST_COUNT = 10;
-	public static final String GALLERY_CREATE_UPDATE_PROGRESS = "Progress Update";
-
 }
