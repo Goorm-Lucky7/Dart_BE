47db9b0f1340c78987913c6a197afd11bc687f0a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-07-02 16:21:45 +0900

    refactor: 불필요한 로깅 제거 및 final 키워드 할당

--- src/main/java/com/dart/api/domain/notification/repository/SSESessionRepository.java
+++ src/main/java/com/dart/api/domain/notification/repository/SSESessionRepository.java
@@ -20,9 +20,8 @@ public class SSESessionRepository {
 	public final Map<Long, SseEmitter> sseSessionDB = new ConcurrentHashMap<>();
 
 	public SseEmitter saveSSEEmitter(Long clientId, long timeout) {
-		SseEmitter sseEmitter = new SseEmitter(timeout);
+		final SseEmitter sseEmitter = new SseEmitter(timeout);
 		sseSessionDB.put(clientId, sseEmitter);
-		log.info("[✅ LOGGER] SAVE SSE EMITTER FOR CLIENT ID: {}", clientId);
 
 		handleSSEEmitter(sseEmitter, clientId);
 
@@ -34,14 +33,12 @@ public class SSESessionRepository {
 
 		if (sseEmitter != null) {
 			try {
-				SseEmitter.SseEventBuilder sseEventBuilder = SseEmitter.event()
+				final SseEmitter.SseEventBuilder sseEventBuilder = SseEmitter.event()
 					.name(SSE_EMITTER_EVENT_NAME)
 					.data(notificationReadDto, MediaType.APPLICATION_JSON);
 
-				log.info("[✅ LOGGER] SENDING EVENT: {}", notificationReadDto);
 				sseEmitter.send(sseEventBuilder);
 			} catch (Exception e) {
-				log.error("[✅ LOGGER] ERROR SENDING EVENT TO CLIENT {}: {}", clientId, e.getMessage());
 				deleteSSEEmitterByClientId(clientId);
 			}
 		}
@@ -71,7 +68,7 @@ public class SSESessionRepository {
 	}
 
 	public void completeSSEEmitter(Long clientId) {
-		SseEmitter sseEmitter = sseSessionDB.get(clientId);
+		final SseEmitter sseEmitter = sseSessionDB.get(clientId);
 		if (sseEmitter != null) {
 			sseEmitter.complete();
 		}
