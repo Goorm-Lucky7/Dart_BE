9e312d639332b734f2ecd09a579d1bcbef2f4e1e
Yeeun Shim <ner.hesed@gmail.com>
2024-07-05 15:27:41 +0900

    feat: autocomplete 반환하는 dto 추가

--- src/main/java/com/dart/api/application/gallery/AutocompleteService.java
+++ src/main/java/com/dart/api/application/gallery/AutocompleteService.java
@@ -1,11 +1,10 @@
 package com.dart.api.application.gallery;
 
-import java.util.List;
-
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.dart.api.domain.gallery.repository.AutocompleteRedisRepository;
+import com.dart.api.dto.gallery.response.AutocompleteResDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,8 +15,7 @@ public class AutocompleteService {
 	private final AutocompleteRedisRepository autocompleteRedisRepository;
 
 	@Cacheable(value = "autocompleteCache", key = "#category + '_' + #keyword")
-	public List<String> autocomplete(String category, String keyword) {
-		List<String> results = autocompleteRedisRepository.search(category, keyword);
-		return results;
+	public AutocompleteResDto autocomplete(String category, String keyword) {
+		return new AutocompleteResDto(autocompleteRedisRepository.search(category, keyword));
 	}
 }
---
+++ src/main/java/com/dart/api/dto/gallery/response/AutocompleteResDto.java
@@ -0,0 +1,8 @@
+package com.dart.api.dto.gallery.response;
+
+import java.util.List;
+
+public record AutocompleteResDto (
+	List<String> result
+) {
+}
