de44ea52c00fd9899d395f8409fa2415205d68a7
Yeeun Shim <ner.hesed@gmail.com>
2024-07-05 21:13:15 +0900

    feat: 토큰 레포지토리에 refresh token 메소드 생성

--- src/main/java/com/dart/api/domain/auth/repository/TokenRedisRepository.java
+++ src/main/java/com/dart/api/domain/auth/repository/TokenRedisRepository.java
@@ -1,10 +1,9 @@
 package com.dart.api.domain.auth.repository;
 
+import static com.dart.global.common.util.AuthConstant.*;
 import static com.dart.global.common.util.RedisConstant.*;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.dart.api.infrastructure.redis.ValueRedisRepository;
 
@@ -16,22 +15,28 @@ public class TokenRedisRepository {
 
 	private final ValueRedisRepository valueRedisRepository;
 
-	public void saveAccessToken(String key, String data, long accessTokenExpire) {
-		if (accessTokenExpire / 1000 >= 1.0) {
-			valueRedisRepository.saveValueWithExpiry(REDIS_ACCESS_TOKEN_PREFIX + key, data, accessTokenExpire / 1000);
-		}
+	public void saveAccessToken(String key, String data) {
+		valueRedisRepository.saveValueWithExpiry(REDIS_ACCESS_TOKEN_PREFIX + key, data, ACCESS_TOKEN_EXPIRATION_TIME_SECONDS);
 	}
 
 	public void saveBlacklistToken(String email, String token) {
 		valueRedisRepository.saveValue(REDIS_BLACKLIST_TOKEN_PREFIX + email, token);
 	}
 
-	public String getAccessToken(String key) {
-		return valueRedisRepository.getValue(REDIS_ACCESS_TOKEN_PREFIX + key);
+	public void saveRefreshToken(String key, String data) {
+		valueRedisRepository.saveValueWithExpiry(REDIS_REFRESH_TOKEN_PREFIX + key, data, REFRESH_TOKEN_EXPIRATION_TIME_SECONDS);
+	}
+
+	public String getAccessToken(String email) {
+		return valueRedisRepository.getValue(REDIS_ACCESS_TOKEN_PREFIX + email);
 	}
 
 	public String getBlacklistToken(String email) {
 		return valueRedisRepository.getValue(REDIS_BLACKLIST_TOKEN_PREFIX + email);
+	}
+
+	public String getRefreshToken(String email) {
+		return valueRedisRepository.getValue(REDIS_REFRESH_TOKEN_PREFIX + email);
 	}
 
 	public void deleteAccessToken(String email) {
@@ -42,7 +47,15 @@ public class TokenRedisRepository {
 		valueRedisRepository.deleteValue(REDIS_BLACKLIST_TOKEN_PREFIX + email);
 	}
 
+	public void deleteRefreshToken(String email) {
+		valueRedisRepository.deleteValue(REDIS_REFRESH_TOKEN_PREFIX + email);
+	}
+
 	public boolean checkAccessTokenExists(String email) {
 		return valueRedisRepository.isValueExists(REDIS_ACCESS_TOKEN_PREFIX + email);
+	}
+
+	public boolean checkRefreshTokenExists(String email) {
+		return valueRedisRepository.isValueExists(REDIS_REFRESH_TOKEN_PREFIX + email);
 	}
 }
