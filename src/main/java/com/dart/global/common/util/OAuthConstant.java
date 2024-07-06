c8bf1d172ddd460e5d5b98337e81bb6e4e777ccd
Yeeun Shim <ner.hesed@gmail.com>
2024-07-06 19:03:11 +0900

    feat: 소셜 로그인 상수 추가

---
+++ src/main/java/com/dart/global/common/util/OAuthConstant.java
@@ -0,0 +1,11 @@
+package com.dart.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class OAuthConstant {
+
+	public static final String KAKAO = "kakao";
+	public static final String GOOGLE = "google";
+}
