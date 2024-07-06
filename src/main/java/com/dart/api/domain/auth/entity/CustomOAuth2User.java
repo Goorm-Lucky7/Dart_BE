d6ed80a35cfd9429e28053cfa53e8bf7e5142558
Yeeun Shim <ner.hesed@gmail.com>
2024-07-06 19:01:12 +0900

    feat: CustomOAuth2User 추가

---
+++ src/main/java/com/dart/api/domain/auth/entity/CustomOAuth2User.java
@@ -0,0 +1,41 @@
+package com.dart.api.domain.auth.entity;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import com.dart.api.domain.member.entity.Member;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+	private final OAuth2User oauth2User;
+	private final String oauthProvider;
+	private final Member member;
+	private final boolean isNewUser;
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		Map<String, Object> attributes = new HashMap<>(oauth2User.getAttributes());
+		attributes.put("member", member);
+		attributes.put("oauthProvider", oauthProvider);
+		return attributes;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return oauth2User.getAuthorities();
+	}
+
+	@Override
+	public String getName() {
+		return oauth2User.getName();
+	}
+}
