365e3d4c6bf01f94b7bc8fc386782339598584ad
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-07-10 17:21:23 +0900

    refactor: 채팅메시지가 레디스에 저장되는 시간을 48시간으로 수정

--- src/main/java/com/dart/global/common/util/ChatConstant.java
+++ src/main/java/com/dart/global/common/util/ChatConstant.java
@@ -12,7 +12,7 @@ public class ChatConstant {
 	public static final String ALLOWED_ORIGIN_PATTERN = "*";
 	public static final String CHAT_SESSION_USER = "authUser";
 	public static final String TOPIC_PREFIX = "/sub/ws/";
-	public static final long CHAT_MESSAGE_EXPIRY_SECONDS = 60 * 60;
+	public static final long CHAT_MESSAGE_EXPIRY_SECONDS = 48 * 60 * 60;
 	public static final String CHAT_BATCH_SAVE_INTERVAL = "0 */5 * * * ?";
 
 	public static final int MESSAGE_SIZE_LIMIT = 160 * 64 * 1024;
