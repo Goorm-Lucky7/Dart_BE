9c2f324cdfd76cd306703b2f1bc699dd2e8ec224
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-07-10 18:33:40 +0900

    refactor: 생성 일자의 JSON 포맷 설정

--- src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
+++ src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
@@ -2,6 +2,8 @@ package com.dart.api.dto.chat.request;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -9,6 +11,8 @@ import lombok.Builder;
 public record ChatMessageCreateDto(
 	@Size(max = 50, message = "[❎ ERROR] 메시지 내용은 50자 이내여야 합니다.")
 	String content,
+
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	LocalDateTime createdAt,
 	String sender,
 	String profileImageUrl,
