1b74537b7ef8e23812bdd02335964969bcadb239
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-07-10 21:19:45 +0900

    refactor: JSON 날짜 포맷 어노테이션 제거

--- src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
+++ src/main/java/com/dart/api/dto/chat/request/ChatMessageCreateDto.java
@@ -11,8 +11,6 @@ import lombok.Builder;
 public record ChatMessageCreateDto(
 	@Size(max = 50, message = "[❎ ERROR] 메시지 내용은 50자 이내여야 합니다.")
 	String content,
-
-	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	LocalDateTime createdAt,
 	String sender,
 	String profileImageUrl,
